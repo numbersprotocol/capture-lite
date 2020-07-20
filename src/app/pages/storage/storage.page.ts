9f66b034d5daa2f5aa98668cc57aadf8a9be0924
Sean Wu <seanwu1105@gmail.com>
2020-07-20 16:03:46 +0800

    Display the thumb of proof on storage page.

--- src/app/pages/storage/storage.page.ts
+++ src/app/pages/storage/storage.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { switchMap, tap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { concatMap, map, switchMap, tap } from 'rxjs/operators';
 import { CameraService } from 'src/app/services/camera/camera.service';
 import { CollectorService } from 'src/app/services/collector/collector.service';
 import { ProofRepository } from 'src/app/services/data/proof/proof-repository.service';
@@ -14,7 +15,11 @@ import { fromExtension } from 'src/app/utils/mime-type';
 })
 export class StoragePage {
 
-  readonly proofList$ = this.proofRepository.getAll$();
+  readonly proofThumbs$ = this.proofRepository.getAll$().pipe(
+    map(proofSet => [...proofSet]),
+    map(proofArray => proofArray.map(proof => this.proofRepository.getRawFile$(proof))),
+    concatMap(rawFileObservables => forkJoin(rawFileObservables))
+  );
 
   constructor(
     private readonly proofRepository: ProofRepository,
