b3e6b137291947f5cfdd6a907cdace45da9108e7
Sean Wu <seanwu1105@gmail.com>
2020-07-24 22:49:37 +0800

    Replace ionic-native splash screen with capcitor plugin.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
@@ -20,8 +19,6 @@ import { LanguageService } from './services/language/language.service';
 export class AppComponent {
   constructor(
     private readonly platform: Platform,
-    private readonly splashScreen: SplashScreen,
-    private readonly statusBar: StatusBar,
     private readonly collectorService: CollectorService,
     private readonly informationRepository: InformationRepository,
     private readonly signatureRepository: SignatureRepository,
@@ -35,8 +32,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      SplashScreen.hide();
     });
   }
 
