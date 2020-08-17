199f95140592b741cfa644ac6a0ddc1f99cb0043
ethanwu155 <et.wuzz@gmail.com>
2020-08-17 15:45:07 +0800

    added enum importance & informationtype

--- src/app/services/data/information/information.ts
+++ src/app/services/data/information/information.ts
@@ -1,8 +1,19 @@
+export const enum Importance {
+  Low = 'low',
+  High = 'high'
+}
+
+export const enum InformationType {
+  Device = 'device',
+  Location = 'location',
+  Other = 'other'
+}
+
 export interface Information {
   readonly proofHash: string;
   readonly provider: string;
   readonly name: string;
   readonly value: string;
-  readonly important: boolean;
-  readonly type: number;
+  readonly important: Importance;
+  readonly type: InformationType;
 }
