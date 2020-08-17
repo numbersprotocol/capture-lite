3f4b84d0f01002e6a416d8fbdfe0e5497b028e4d
ethanwu155 <et.wuzz@gmail.com>
2020-08-17 15:43:16 +0800

    edited information.page

--- src/app/pages/information/information.page.ts
+++ src/app/pages/information/information.page.ts
@@ -1,72 +1,54 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { map, switchMap, switchMapTo } from 'rxjs/operators';
+import { map, pluck, switchMap, switchMapTo } from 'rxjs/operators';
+import { InformationType } from 'src/app/services/data/information/information';
 import { InformationRepository } from 'src/app/services/data/information/information-repository.service';
 import { ProofRepository } from 'src/app/services/data/proof/proof-repository.service';
 import { isNonNullable } from 'src/app/utils/rx-operators';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
-  selector: 'app-information',
-  templateUrl: './information.page.html',
-  styleUrls: ['./information.page.scss'],
+    selector: 'app-information',
+    templateUrl: './information.page.html',
+    styleUrls: ['./information.page.scss'],
 })
 export class InformationPage {
 
-  readonly proof$ = this.route.paramMap.pipe(
-    map(params => params.get('hash')),
-    isNonNullable(),
-    switchMap(hash => this.proofRepository.getByHash$(hash)),
-    isNonNullable()
-  );
+    readonly proof$ = this.route.paramMap.pipe(
+        map(params => params.get('hash')),
+        isNonNullable(),
+        switchMap(hash => this.proofRepository.getByHash$(hash)),
+        isNonNullable()
+    );
 
-  readonly hash$ = this.proof$.pipe(map(proof => proof.hash));
+    readonly hash$ = this.proof$.pipe(pluck('hash'));
 
-  readonly locationInformation$ = this.proof$.pipe(
-    switchMap(proof => this.informationRepository.getByProof$(proof)),
-    map(informationList => {
-      const types = new Array(informationList.map(information => information.type));
-      return [...types].map(type => ({
-        type,
-        informationList: informationList.filter(information => information.type === 1)
-      }));
-    })
-  );
+    readonly locationInformation$ = this.proof$.pipe(
+        switchMap(proof => this.informationRepository.getByProof$(proof)),
+        map(informationList => informationList.filter(information => information.type === InformationType.Location))
+    );
 
-  readonly hashInformation$ = this.proof$.pipe(
-    switchMap(proof => this.informationRepository.getByProof$(proof)),
-    map(informationList => {
-      const types = new Array(informationList.map(information => information.type));
-      return [...types].map(type => ({
-        type,
-        informationList: informationList.filter(information => information.type === 2)
-      }));
-    })
-  );
+    readonly otherInformation$ = this.proof$.pipe(
+        switchMap(proof => this.informationRepository.getByProof$(proof)),
+        map(informationList => informationList.filter(information => information.type === InformationType.Other))
+    );
 
-  readonly otherInformation$ = this.proof$.pipe(
-    switchMap(proof => this.informationRepository.getByProof$(proof)),
-    map(informationList => {
-      const types = new Array(informationList.map(information => information.type));
-      return [...types].map(type => ({
-        type,
-        informationList: informationList.filter(information => information.type === 0)
-      }));
-    })
-  );
+    readonly deviceInformation$ = this.proof$.pipe(
+        switchMap(proof => this.informationRepository.getByProof$(proof)),
+        map(informationList => informationList.filter(information => information.type === InformationType.Device))
+    );
 
-  constructor(
-    private readonly router: Router,
-    private readonly route: ActivatedRoute,
-    private readonly proofRepository: ProofRepository,
-    private readonly informationRepository: InformationRepository,
-  ) { }
+    constructor(
+        private readonly route: ActivatedRoute,
+        private readonly proofRepository: ProofRepository,
+        private readonly informationRepository: InformationRepository,
+    ) { }
 
-  ionViewWillEnter() {
-    this.proofRepository.refresh$().pipe(
-      switchMapTo(this.informationRepository.refresh$()),
-      untilDestroyed(this)
-    ).subscribe();
-  }
+    ionViewWillEnter() {
+        this.proofRepository.refresh$().pipe(
+            switchMapTo(this.informationRepository.refresh$()),
+            untilDestroyed(this)
+        ).subscribe();
+    }
 }
