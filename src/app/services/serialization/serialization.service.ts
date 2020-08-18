fdb492ad2cda1f7412273ebb0af007b3598b48db
Sean Wu <seanwu1105@gmail.com>
2020-08-18 15:22:56 +0800

    Make the format of SortedProofInformation to be the same as Starling Capture.

--- src/app/services/serialization/serialization.service.ts
+++ src/app/services/serialization/serialization.service.ts
@@ -4,6 +4,13 @@ import { Information } from '../data/information/information';
 import { InformationRepository } from '../data/information/information-repository.service';
 import { Proof } from '../data/proof/proof';
 
+type EssentialInformation = Pick<Information, 'provider' | 'name' | 'value'>;
+
+interface SortedProofInformation {
+  readonly proof: Proof;
+  readonly information: EssentialInformation[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -24,22 +31,15 @@ export class SerializationService {
       first(),
       map(informationList => {
         const sortedInformation = informationList.sort((a: Information, b: Information) => {
-          const proofHashCompared = a.proofHash.localeCompare(b.proofHash);
           const providerCompared = a.provider.localeCompare(b.provider);
           const nameCompared = a.name.localeCompare(b.name);
           const valueCompared = a.value.localeCompare(b.value);
-          if (proofHashCompared !== 0) { return proofHashCompared; }
           if (providerCompared !== 0) { return providerCompared; }
           if (nameCompared !== 0) { return nameCompared; }
           return valueCompared;
-        });
-        return ({ proof, sortedInformation } as SortedProofInformation);
+        }).map(({ provider, name, value }) => ({ provider, name, value } as EssentialInformation));
+        return ({ proof, information: sortedInformation } as SortedProofInformation);
       })
     );
   }
-}
-
-interface SortedProofInformation {
-  readonly proof: Proof;
-  readonly sortedInformation: Information[];
 }
