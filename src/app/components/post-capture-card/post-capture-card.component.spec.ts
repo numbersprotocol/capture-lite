4cc197641e81ab6cefbfa177b564ec04cdfedbc3
Sean Wu <seanwu1105@gmail.com>
2020-11-04 15:13:18 +0800

    Fix unit tests for post-capture-card component.

--- src/app/components/post-capture-card/post-capture-card.component.spec.ts
+++ src/app/components/post-capture-card/post-capture-card.component.spec.ts
@@ -1,21 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { IonicModule } from '@ionic/angular';
+import { Proof } from 'src/app/services/data/proof/proof';
 import { getTranslocoModule } from 'src/app/transloco/transloco-root.module.spec';
 import { PostCaptureCardComponent } from './post-capture-card.component';
 
 describe('PostCaptureCardComponent', () => {
   let component: PostCaptureCardComponent;
   let fixture: ComponentFixture<PostCaptureCardComponent>;
+  const expectedProof: Proof = { hash: '1234567890abcdef', timestamp: 1604473851208, mimeType: 'image/jpeg' };
+  const expectedImageSrc = 'https://picsum.photos/200/300';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PostCaptureCardComponent],
       imports: [
         IonicModule.forRoot(),
+        HttpClientTestingModule,
         MatCardModule,
         MatButtonModule,
         MatIconModule,
@@ -26,6 +31,8 @@ describe('PostCaptureCardComponent', () => {
 
     fixture = TestBed.createComponent(PostCaptureCardComponent);
     component = fixture.componentInstance;
+    component.proof = expectedProof;
+    component.imageSrc = expectedImageSrc;
     fixture.detectChanges();
   }));
 
