7e72f39869b30c19c52a7e5dc5727c835e09477b
Sean Wu <seanwu1105@gmail.com>
2020-11-10 12:46:44 +0800

    Add options menu to proof page.

--- src/app/pages/storage/proof/proof.module.ts
+++ src/app/pages/storage/proof/proof.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,6 +14,7 @@ import { IonicModule } from '@ionic/angular';
 import { TranslocoModule } from '@ngneat/transloco';
 import { ContactSelectionDialogComponent } from './contact-selection-dialog/contact-selection-dialog.component';
 import { FriendInvitationDialogComponent } from './contact-selection-dialog/friend-invitation-dialog/friend-invitation-dialog.component';
+import { OptionsMenuComponent } from './options-menu/options-menu.component';
 import { ProofPageRoutingModule } from './proof-routing.module';
 import { ProofPage } from './proof.page';
 
@@ -30,12 +32,14 @@ import { ProofPage } from './proof.page';
     MatSnackBarModule,
     MatInputModule,
     MatDialogModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatBottomSheetModule
   ],
   declarations: [
     ProofPage,
     ContactSelectionDialogComponent,
-    FriendInvitationDialogComponent
+    FriendInvitationDialogComponent,
+    OptionsMenuComponent
   ]
 })
 export class ProofPageModule { }
--- src/app/pages/storage/proof/proof.page.ts
+++ src/app/pages/storage/proof/proof.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -6,7 +7,7 @@ import { Plugins } from '@capacitor/core';
 import { TranslocoService } from '@ngneat/transloco';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { defer } from 'rxjs';
-import { map, pluck, switchMap, switchMapTo } from 'rxjs/operators';
+import { map, pluck, switchMap, switchMapTo, tap } from 'rxjs/operators';
 import { BlockingActionService } from 'src/app/services/blocking-action/blocking-action.service';
 import { CapacitorProvider } from 'src/app/services/collector/information/capacitor-provider/capacitor-provider';
 import { ConfirmAlert } from 'src/app/services/confirm-alert/confirm-alert.service';
@@ -15,6 +16,7 @@ import { InformationRepository } from 'src/app/services/data/information/informa
 import { ProofRepository } from 'src/app/services/data/proof/proof-repository.service';
 import { isNonNullable } from 'src/app/utils/rx-operators';
 import { ContactSelectionDialogComponent, SelectedContact } from './contact-selection-dialog/contact-selection-dialog.component';
+import { Option, OptionsMenuComponent } from './options-menu/options-menu.component';
 
 const { Clipboard } = Plugins;
 
@@ -61,7 +63,8 @@ export class ProofPage {
     private readonly informationRepository: InformationRepository,
     private readonly blockingActionService: BlockingActionService,
     private readonly snackBar: MatSnackBar,
-    private readonly dialog: MatDialog
+    private readonly dialog: MatDialog,
+    private readonly bottomSheet: MatBottomSheet
   ) { }
 
   openContactSelectionDialog() {
@@ -78,7 +81,17 @@ export class ProofPage {
       ));
   }
 
-  remove() {
+  openOptionsMenu() {
+    const bottomSheetRef = this.bottomSheet.open(OptionsMenuComponent);
+    bottomSheetRef.afterDismissed().pipe(
+      tap((option?: Option) => {
+        if (option === Option.Delete) { this.remove(); }
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+  }
+
+  private remove() {
     const onConfirm = () => this.blockingActionService.run$(
       this.proof$.pipe(
         switchMap(proof => this.proofRepository.remove$(proof)),
