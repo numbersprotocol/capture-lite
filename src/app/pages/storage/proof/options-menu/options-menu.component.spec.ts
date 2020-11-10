0403b4189eab5cf1b424aeec395948fffcc770ae
Sean Wu <seanwu1105@gmail.com>
2020-11-10 13:27:58 +0800

    Fix tests for FakeMatIconRegistry.

--- src/app/pages/storage/proof/options-menu/options-menu.component.spec.ts
+++ src/app/pages/storage/proof/options-menu/options-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatBottomSheetModule, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
 import { IonicModule } from '@ionic/angular';
-
 import { OptionsMenuComponent } from './options-menu.component';
 
 describe('OptionsMenuComponent', () => {
@@ -9,8 +10,15 @@ describe('OptionsMenuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OptionsMenuComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [OptionsMenuComponent],
+      imports: [
+        IonicModule.forRoot(),
+        MatBottomSheetModule,
+        MatListModule
+      ],
+      providers: [
+        { provide: MatBottomSheetRef, useValue: {} }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(OptionsMenuComponent);
