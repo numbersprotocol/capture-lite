dd2a0bda6a97ed6b638aad0bff07cad8b39a45f1
Sean Wu <seanwu1105@gmail.com>
2020-11-10 12:09:15 +0800

    Align the title to center and remove copy buttons on information page.

--- src/app/pages/storage/proof/information/information.page.ts
+++ src/app/pages/storage/proof/information/information.page.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
-import { Plugins } from '@capacitor/core';
-import { TranslocoService } from '@ngneat/transloco';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { map, switchMap } from 'rxjs/operators';
+import { map, pluck, switchMap } from 'rxjs/operators';
+import { WebCryptoApiProvider } from 'src/app/services/collector/signature/web-crypto-api-provider/web-crypto-api-provider';
 import { InformationType } from 'src/app/services/data/information/information';
 import { InformationRepository } from 'src/app/services/data/information/information-repository.service';
 import { ProofRepository } from 'src/app/services/data/proof/proof-repository.service';
+import { SignatureRepository } from 'src/app/services/data/signature/signature-repository.service';
 import { isNonNullable } from 'src/app/utils/rx-operators';
-
-const { Clipboard } = Plugins;
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -27,6 +24,10 @@ export class InformationPage {
     isNonNullable()
   );
 
+  readonly timestamp$ = this.proof$.pipe(pluck('timestamp'));
+  readonly hash$ = this.proof$.pipe(pluck('hash'));
+  readonly mimeType$ = this.proof$.pipe(pluck('mimeType'));
+
   readonly locationInformation$ = this.proof$.pipe(
     switchMap(proof => this.informationRepository.getByProof$(proof)),
     map(informationList => informationList.filter(information => information.type === InformationType.Location))
@@ -42,16 +43,16 @@ export class InformationPage {
     map(informationList => informationList.filter(information => information.type === InformationType.Other))
   );
 
+  readonly signature$ = this.proof$.pipe(
+    switchMap(proof => this.signatureRepository.getByProof$(proof)),
+    map(signatures => signatures.find(signature => signature.provider === WebCryptoApiProvider.ID)),
+    isNonNullable()
+  );
+
   constructor(
     private readonly route: ActivatedRoute,
     private readonly proofRepository: ProofRepository,
     private readonly informationRepository: InformationRepository,
-    private readonly translocoService: TranslocoService,
-    private readonly snackBar: MatSnackBar,
+    private readonly signatureRepository: SignatureRepository
   ) { }
-
-  copyToClipboard(value: string) {
-    Clipboard.write({ string: value });
-    this.snackBar.open(this.translocoService.translate('message.copiedToClipboard'));
-  }
 }
--- src/app/pages/storage/proof/proof.page.ts
+++ src/app/pages/storage/proof/proof.page.ts
@@ -9,12 +9,10 @@ import { defer } from 'rxjs';
 import { map, pluck, switchMap, switchMapTo } from 'rxjs/operators';
 import { BlockingActionService } from 'src/app/services/blocking-action/blocking-action.service';
 import { CapacitorProvider } from 'src/app/services/collector/information/capacitor-provider/capacitor-provider';
-import { WebCryptoApiProvider } from 'src/app/services/collector/signature/web-crypto-api-provider/web-crypto-api-provider';
 import { ConfirmAlert } from 'src/app/services/confirm-alert/confirm-alert.service';
 import { CaptionRepository } from 'src/app/services/data/caption/caption-repository.service';
 import { InformationRepository } from 'src/app/services/data/information/information-repository.service';
 import { ProofRepository } from 'src/app/services/data/proof/proof-repository.service';
-import { SignatureRepository } from 'src/app/services/data/signature/signature-repository.service';
 import { isNonNullable } from 'src/app/utils/rx-operators';
 import { ContactSelectionDialogComponent, SelectedContact } from './contact-selection-dialog/contact-selection-dialog.component';
 
@@ -45,19 +43,12 @@ export class ProofPage {
       return '';
     })
   );
-  readonly hash$ = this.proof$.pipe(pluck('hash'));
   readonly timestamp$ = this.proof$.pipe(pluck('timestamp'));
-  readonly mimeType$ = this.proof$.pipe(pluck('mimeType'));
   readonly location$ = this.proof$.pipe(
     switchMap(proof => this.informationRepository.getByProof$(proof)),
     map(informationList => informationList.find(information => information.provider === CapacitorProvider.ID && information.name === 'Location')),
     isNonNullable(),
     pluck('value')
-  );
-  readonly signature$ = this.proof$.pipe(
-    switchMap(proof => this.signatureRepository.getByProof$(proof)),
-    map(signatures => signatures.find(signature => signature.provider === WebCryptoApiProvider.ID)),
-    isNonNullable()
   );
 
   constructor(
@@ -68,7 +59,6 @@ export class ProofPage {
     private readonly proofRepository: ProofRepository,
     private readonly captionRepository: CaptionRepository,
     private readonly informationRepository: InformationRepository,
-    private readonly signatureRepository: SignatureRepository,
     private readonly blockingActionService: BlockingActionService,
     private readonly snackBar: MatSnackBar,
     private readonly dialog: MatDialog
