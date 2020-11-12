d522135c0dbbcd8fc06997c11151b2bd6953fd28
Sean Wu <seanwu1105@gmail.com>
2020-11-12 21:42:04 +0800

    Move subscription from background to foreground. Fix #186.
    When we do task inside the Capacitor Plugin BackgroundTask, the task
    will NOT stay inside the ngZone, and thus cause the observables observed
    with async pipe fail to watch new emitted values.

--- src/app/services/publisher/publisher.ts
+++ src/app/services/publisher/publisher.ts
@@ -1,7 +1,6 @@
 import { TranslocoService } from '@ngneat/transloco';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { subscribeInBackground } from 'src/app/utils/background-task/background-task';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { Proof } from '../data/proof/proof';
 import { NotificationService } from '../notification/notification.service';
 
@@ -21,16 +20,18 @@ export abstract class Publisher {
       this.translocoService.translate('publishingProof'),
       this.translocoService.translate('message.publishingProof', { hash: proof.hash, publisherName: this.id })
     );
-    subscribeInBackground(
-      this.run$(proof).pipe(
-        tap(_ => this.notificationService.notify(
-          notificationId,
-          this.translocoService.translate('proofPublished'),
-          this.translocoService.translate('message.proofPublished', { hash: proof.hash, publisherName: this.id })
-        ))
-      ),
-      error => this.notificationService.notifyError(notificationId, error)
-    );
+
+    // Deliberately subscribe without untilDestroyed scope. Also, it is not feasible to use
+    // subsctibeInBackground() as it will move the execution out of ngZone, which will prevent
+    // the observables subscribed with async pipe from observing new values.
+    this.run$(proof).pipe(
+      tap(_ => this.notificationService.notify(
+        notificationId,
+        this.translocoService.translate('proofPublished'),
+        this.translocoService.translate('message.proofPublished', { hash: proof.hash, publisherName: this.id })
+      )),
+      catchError((err: Error) => of(this.notificationService.notifyError(notificationId, err)))
+    ).subscribe();
   }
 
   abstract isEnabled$(): Observable<boolean>;
