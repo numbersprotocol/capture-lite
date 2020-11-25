4a61d42761e315ee5f277de3e3c33966c16cebce
DaYuan <sduum82@gmail.com>
2020-11-25 15:13:39 +0800

    add JSX+CSS solution

--- src/app/shared/post-capture-card/post-capture-card.component.ts
+++ src/app/shared/post-capture-card/post-capture-card.component.ts
@@ -1,8 +1,6 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Asset } from 'src/app/services/publisher/numbers-storage/data/asset/asset';
 import { Transaction } from 'src/app/services/publisher/numbers-storage/numbers-storage-api.service';
-const PREFIX = '--';
-
 @Component({
   selector: 'app-post-capture-card',
   templateUrl: './post-capture-card.component.html',
@@ -15,8 +13,6 @@ export class PostCaptureCardComponent implements OnInit {
 
   latitude!: string;
   longitude!: string;
-
-
   openMore = false;
 
   ngOnInit() {
@@ -24,37 +20,6 @@ export class PostCaptureCardComponent implements OnInit {
     this.longitude = this.asset.information.information.find(info => info.name === 'Current GPS Longitude')?.value || 'unknown';
   }
 
-
-  constructor() {
-    // this.changeTheme(); // Set default theme
-  }
-
-  changeTheme() {
-    var height = this.myIdentifier.nativeElement.offsetHeight;
-    console.log('HeightHeight: ' + height);
-
-    document.documentElement.style.setProperty('--ts-img', height);
-  }
-
-
-  ngAfterViewInit() {
-    var width = this.myIdentifier.nativeElement.offsetWidth;
-    var height = this.myIdentifier.nativeElement.offsetHeight;
-    // ResizeObserve
-    console.log('Width:' + width);
-    console.log('Height: ' + height);
-    // clientHeight
-    // height
-    console.log('myIdentifier: ', this.myIdentifier);
-
-  }
-  readProperty(name: string): string {
-    const bodyStyles = window.getComputedStyle(document.body);
-    return bodyStyles.getPropertyValue(PREFIX + name);
-  }
-
   @ViewChild('myIdentifier')
   myIdentifier!: ElementRef;
-
-
 }
