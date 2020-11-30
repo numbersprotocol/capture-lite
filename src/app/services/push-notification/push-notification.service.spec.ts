96993882623043a2b5d7419abf206e44b3a6ccf6
Sean Wu <seanwu1105@gmail.com>
2020-11-30 11:12:26 +0800

    Fix tests.

--- src/app/services/push-notification/push-notification.service.spec.ts
+++ src/app/services/push-notification/push-notification.service.spec.ts
@@ -1,12 +1,13 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { TranslocoModule } from '@ngneat/transloco';
+import { getTranslocoModule } from 'src/app/transloco/transloco-root.module.spec';
 import { PushNotificationService } from './push-notification.service';
 
 describe('PushNotificationService', () => {
   let service: PushNotificationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [TranslocoModule] });
+    TestBed.configureTestingModule({ imports: [getTranslocoModule(), HttpClientTestingModule] });
     service = TestBed.inject(PushNotificationService);
   });
 
