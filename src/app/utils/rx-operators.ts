6ddbf45bbd8e459229fc1e55898f45028a8f8804
Sean Wu <seanwu1105@gmail.com>
2020-12-07 22:36:29 +0800

    Remove unused rx-operators.

--- src/app/utils/rx-operators.ts
+++ src/app/utils/rx-operators.ts
@@ -1,17 +1,9 @@
-import { forkJoin, Observable } from 'rxjs';
-import { defaultIfEmpty, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 export function isNonNullable<T>() {
   return (source$: Observable<null | undefined | T>) =>
     source$.pipe(
       filter((v): v is NonNullable<T> => v !== null && v !== undefined)
     );
-}
-
-// TODO: Remove this function as we have promisify most utils methods. See #233.
-export function forkJoinWithDefault<T>(
-  sources: Observable<T>[],
-  defaultValue: T[] = []
-) {
-  return forkJoin(sources).pipe(defaultIfEmpty(defaultValue));
 }
