3a9fc5eb37af9c3e24e4daa18d29b39e67b927fd
DaYuan <sduum82@gmail.com>
2020-12-08 01:00:27 +0800

    details =>details$  Observable

--- src/app/pages/home/activity/transaction-details/transaction-details.page.ts
+++ src/app/pages/home/activity/transaction-details/transaction-details.page.ts
@@ -1,28 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { map } from 'rxjs/operators';
-import { Transaction } from '../../../../services/publisher/numbers-storage/numbers-storage-api.service';
-
 @UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-transaction-details',
   templateUrl: './transaction-details.page.html',
   styleUrls: ['./transaction-details.page.scss'],
 })
-export class TransactionDetailsPage implements OnInit {
-  details!: Transaction;
+export class TransactionDetailsPage {
+  details$ = this.route.paramMap.pipe(map(() => history.state));
 
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute
   ) {}
-
-  ngOnInit() {
-    this.route.paramMap
-      .pipe(map(() => window.history.state))
-      .subscribe(details => {
-        this.details = details;
-      });
-  }
 }
