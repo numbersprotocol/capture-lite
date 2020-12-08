faf8ef50fdda9592c7d7c6b80d6df00548349b56
Sean Wu <seanwu1105@gmail.com>
2020-12-09 00:43:20 +0800

    Show contact list on contact selection dialog. #307
    * Remove unused rx-operators.
    * Implement DiaBackendContactRepository.
    * Show contacts on contact-selection-dialog.
    * Show progress spinner when fetching contacts with empty cache.
    * Add argument for comparator to Table.insert method.
    * Implement OnConflictStrategy.REPLACE for Table.insert.
    * Implement comparator for Table.delete method.

---
+++ src/app/services/dia-backend/contact/dia-backend-contact-repository.service.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { SharedModule } from '../../../shared/shared.module';
+import { DiaBackendAuthService } from '../auth/dia-backend-auth.service';
+import { DiaBackendContactRepository } from './dia-backend-contact-repository.service';
+
+xdescribe('DiaBackendContactRepository', () => {
+  let repository: DiaBackendContactRepository;
+  let authService: DiaBackendAuthService;
+
+  beforeEach(done => {
+    TestBed.configureTestingModule({
+      imports: [SharedModule],
+    });
+    repository = TestBed.inject(DiaBackendContactRepository);
+    authService = TestBed.inject(DiaBackendAuthService);
+    authService.login$(EMAIL, PASSWORD).subscribe(done);
+  });
+
+  afterEach(done => authService.logout$().subscribe(done));
+
+  it('should be created', () => expect(repository).toBeTruthy());
+
+  it('should get all local cache first then via HTTP request', done => {
+    // tslint:disable-next-line: no-console
+    repository.getAll$().subscribe(result => console.log(result));
+  });
+});
+
+const EMAIL = 'test@test.com';
+const PASSWORD = 'testpassword';
