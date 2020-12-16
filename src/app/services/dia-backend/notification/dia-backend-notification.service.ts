6c0315d51be12666b8731c4e68d536aebe2276b3
James Chien <shc261392@gmail.com>
2020-12-16 12:51:32 +0800

    Remove local notification message in push notification.
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/services/dia-backend/notification/dia-backend-notification.service.ts
+++ src/app/services/dia-backend/notification/dia-backend-notification.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
-import { TranslocoService } from '@ngneat/transloco';
-import { defer, EMPTY, Observable } from 'rxjs';
+import { EMPTY, forkJoin, Observable } from 'rxjs';
 import { concatMap, filter } from 'rxjs/operators';
-import { switchTapTo } from '../../../utils/rx-operators/rx-operators';
-import { NotificationService } from '../../notification/notification.service';
 import { PushNotificationService } from '../../push-notification/push-notification.service';
 import { DiaBackendAssetRepository } from '../asset/dia-backend-asset-repository.service';
 import { DiaBackendTransactionRepository } from '../transaction/dia-backend-transaction-repository.service';
@@ -15,9 +12,7 @@ export class DiaBackendNotificationService {
   constructor(
     private readonly pushNotificationService: PushNotificationService,
     private readonly transactionRepository: DiaBackendTransactionRepository,
-    private readonly assetRepositroy: DiaBackendAssetRepository,
-    private readonly notificationService: NotificationService,
-    private readonly translocoService: TranslocoService
+    private readonly assetRepositroy: DiaBackendAssetRepository
   ) {}
 
   initialize$() {
@@ -25,23 +20,13 @@ export class DiaBackendNotificationService {
       isDiaBackendPushNotificationData(),
       concatMap(data => {
         if (data.app_message_type === 'transaction_received') {
-          return defer(() =>
-            this.notificationService.notify(
-              this.translocoService.translate('transactionReceived'),
-              this.translocoService.translate('message.transactionReceived')
-            )
-          ).pipe(switchTapTo(this.transactionRepository.refresh$()));
+          return this.transactionRepository.refresh$();
         }
         if (data.app_message_type === 'transaction_expired') {
-          return defer(() =>
-            this.notificationService.notify(
-              this.translocoService.translate('transactionExpired'),
-              this.translocoService.translate('message.transactionExpired')
-            )
-          ).pipe(
-            switchTapTo(this.transactionRepository.refresh$()),
-            switchTapTo(this.assetRepositroy.refresh$())
-          );
+          return forkJoin([
+            this.transactionRepository.refresh$(),
+            this.assetRepositroy.refresh$(),
+          ]);
         }
         return EMPTY;
       })
