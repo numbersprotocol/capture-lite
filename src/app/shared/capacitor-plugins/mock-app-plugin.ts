bcb93ea70b6cdde13f182b5991398d39cf037a37
James Chien <shc261392@gmail.com>
2020-12-16 20:50:01 +0800

    Add App plugin for checking App state.
    Signed-off-by: James Chien <shc261392@gmail.com>

---
+++ src/app/shared/capacitor-plugins/mock-app-plugin.ts
@@ -0,0 +1,37 @@
+// tslint:disable: prefer-function-over-method no-async-without-await
+import {
+  AppLaunchUrl,
+  AppPlugin,
+  AppState,
+  PluginListenerHandle,
+} from '@capacitor/core';
+
+export class MockAppPlugin implements AppPlugin {
+  addListener(): PluginListenerHandle {
+    return { remove: () => {} };
+  }
+
+  removeAllListeners(): void {}
+
+  exitApp(): never {
+    throw new Error('exited');
+  }
+
+  async canOpenUrl(options: { url: string }): Promise<{ value: boolean }> {
+    return Promise.resolve({ value: true });
+  }
+
+  async getLaunchUrl(): Promise<AppLaunchUrl> {
+    return Promise.resolve({ url: '' });
+  }
+
+  async getState(_options?: any): Promise<AppState> {
+    return {
+      isActive: true,
+    };
+  }
+
+  async openUrl(options: { url: string }): Promise<{ completed: boolean }> {
+    return Promise.resolve({ completed: true });
+  }
+}
