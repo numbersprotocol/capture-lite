1dde14446aef463f87b62dc1ec5277dd1319370d
DaYuan <sduum82@gmail.com>
2020-12-28 15:18:24 +0800

    rm Unnecessary function

--- src/app/pages/tutorial/tutorial.page.ts
+++ src/app/pages/tutorial/tutorial.page.ts
@@ -9,21 +9,9 @@ import { OnboardingService } from '../../services/onboarding/onboarding.service'
   styleUrls: ['./tutorial.page.scss'],
 })
 export class TutorialPage implements OnInit {
-  slideOptsOne = {
-    initialSlide: 0,
-    slidesPerView: 1,
-  };
-
   constructor(private readonly onboardingService: OnboardingService) {}
 
   ngOnInit() {
     this.onboardingService.onboard();
-  }
-
-  // tslint:disable-next-line: prefer-function-over-method prefer-method-signature no-inferred-empty-object-type
-  next(slide: { slideTo: (arg0: number) => {} }, index: number) {
-    // @ts-ignore
-    // tslint:disable-next-line: no-inferred-empty-object-type
-    slide.slideTo(index);
   }
 }
