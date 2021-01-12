0d69b33f9c206141f0428a96c215ee94476b799e
Sean Wu <seanwu1105@gmail.com>
2021-01-12 16:15:00 +0800

    Display local facts of a Proof on information page.

--- src/app/features/home/capture-tab/capture-details/information/information.page.ts
+++ src/app/features/home/capture-tab/capture-details/information/information.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { getOldProof } from '../../../../../shared/services/repositories/proof/old-proof-adapter';
+import { ProofRepository } from '../../../../../shared/services/repositories/proof/proof-repository.service';
 import { isNonNullable } from '../../../../../utils/rx-operators/rx-operators';
 
 @UntilDestroy({ checkProperties: true })
@@ -11,11 +13,29 @@ import { isNonNullable } from '../../../../../utils/rx-operators/rx-operators';
   styleUrls: ['./information.page.scss'],
 })
 export class InformationPage {
-  readonly asset$ = this.route.paramMap.pipe(
+  readonly proof$ = this.route.paramMap.pipe(
     map(params => params.get('id')),
+    isNonNullable(),
+    switchMap(id => this.proofRepository.getById$(id)),
     isNonNullable(),
     shareReplay({ bufferSize: 1, refCount: true })
   );
+  readonly timestamp$ = this.proof$.pipe(map(proof => proof?.timestamp));
+  readonly oldProofHash$ = this.proof$.pipe(
+    map(proof => getOldProof(proof).hash)
+  );
+  readonly mimeType$ = this.proof$.pipe(
+    map(proof => Object.values(proof.indexedAssets)[0].mimeType)
+  );
+  readonly facts$ = this.proof$.pipe(
+    map(proof => Object.values(proof.truth.providers)[0])
+  );
+  readonly signature$ = this.proof$.pipe(
+    map(proof => Object.values(proof.signatures)[0])
+  );
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly proofRepository: ProofRepository
+  ) {}
 }
