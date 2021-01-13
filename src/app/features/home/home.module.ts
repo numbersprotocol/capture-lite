be590113dff76b4a03e7212cf93017ffcb13538f
James Chien <shc261392@gmail.com>
2021-01-13 12:47:00 +0800

    Add upload-bar to home page
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { PostCaptureCardModule } from '../../shared/core/post-capture-card/post-capture-card.module';
 import { SharedModule } from '../../shared/shared.module';
 import { CaptureTabComponent } from './capture-tab/capture-tab.component';
+import { UploadBarModule } from './capture-tab/upload-bar/upload-bar.module';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [SharedModule, HomePageRoutingModule, PostCaptureCardModule],
+  imports: [
+    SharedModule,
+    HomePageRoutingModule,
+    PostCaptureCardModule,
+    UploadBarModule,
+  ],
   declarations: [HomePage, CaptureTabComponent],
 })
 export class HomePageModule {}
