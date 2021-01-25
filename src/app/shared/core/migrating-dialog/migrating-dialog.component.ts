e67010fe89a89a79df3673917074afbea32b3593
James Chien <shc261392@gmail.com>
2021-01-26 03:49:59 +0800

    Remove unused injection token
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/shared/core/migrating-dialog/migrating-dialog.component.ts
+++ src/app/shared/core/migrating-dialog/migrating-dialog.component.ts
@@ -1,11 +1,8 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-migrating-dialog',
   templateUrl: './migrating-dialog.component.html',
   styleUrls: ['./migrating-dialog.component.scss'],
 })
-export class MigratingDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { name: string }) {}
-}
+export class MigratingDialogComponent {}
