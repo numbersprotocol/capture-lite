c72a72b3216ee8c8bab056c5828d9ade3c85adba
shc261392 <shc261392@gmail.com>
2021-02-18 17:02:41 +0800

    Add CI/CD for iOS (#544)
    * Use manual signing and distribution signing identity for iOS build
    
    * Add ExportOptions.plist
    
    * Add iOS debug build in build workflow
    
    * Remove iOS upload artifacts steps
    Artifact has storage limits and is not suitable for large files. And since iOS
    archives and IPAs suited only for debug purpose, uploading it wouldn't
    be as useful as uploading an apk, so the uploading steps are removed.
    
    * Run iOS build as standalone job and add missing job prerequisites
    
    * Apply workaround to prevent chrome disconnection issue
    
    Ref: https://github.com/karma-runner/karma-chrome-launcher/issues/154#issuecomment-569698577
    
    Co-authored-by: Sean Wu <seanwu1105@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
+        flags: ['--no-sandbox', '--js-flags=--max-old-space-size=4096'],
       },
     },
     singleRun: false,
