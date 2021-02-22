303e8331f372f837a93c36c361230b912608258a
Sean Wu <seanwu1105@gmail.com>
2021-02-22 14:06:51 +0800

    Implement the password-resetting dialog. (#549)
    * Implement the password-resetting dialog.
    
    * Move the resetting-password feature to login page.

--- src/app/shared/services/dia-backend/auth/dia-backend-auth.service.ts
+++ src/app/shared/services/dia-backend/auth/dia-backend-auth.service.ts
@@ -126,10 +126,22 @@ export class DiaBackendAuthService {
     });
   }
 
-  resendActivationEmail(email: string) {
-    return this.httpClient.post(`${BASE_URL}/auth/users/resend_activation/`, {
-      email,
-    });
+  resendActivationEmail$(email: string) {
+    return this.httpClient.post<ResendActivationEmailResponse>(
+      `${BASE_URL}/auth/users/resend_activation/`,
+      {
+        email,
+      }
+    );
+  }
+
+  resetPassword$(email: string) {
+    return this.httpClient.post<ResetPasswordResponse>(
+      `${BASE_URL}/auth/users/reset_password/`,
+      {
+        email,
+      }
+    );
   }
 
   updateLanguage$(headers: { [header: string]: string | string[] }) {
@@ -229,3 +241,9 @@ export interface ReadUserResponse {
 
 // tslint:disable-next-line: no-empty-interface
 interface CreateUserResponse {}
+
+// tslint:disable-next-line: no-empty-interface
+interface ResendActivationEmailResponse {}
+
+// tslint:disable-next-line: no-empty-interface
+interface ResetPasswordResponse {}
