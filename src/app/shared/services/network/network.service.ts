1ea080df3a31b4af30f3f06399825a02772825c6
Sean Wu <seanwu1105@gmail.com>
2021-03-30 11:45:10 +0800

    Show network status bar on PostCapture tab.

--- src/app/shared/services/network/network.service.ts
+++ src/app/shared/services/network/network.service.ts
@@ -1,31 +1,27 @@
 import { Inject, Injectable, NgZone } from '@angular/core';
 import { NetworkPlugin, NetworkStatus } from '@capacitor/core';
-import { BehaviorSubject } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { defer, merge, ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, pluck } from 'rxjs/operators';
 import { NETOWRK_PLUGIN } from '../../core/capacitor-plugins/capacitor-plugins.module';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NetworkService {
-  private readonly _connected$ = new BehaviorSubject(true);
-  readonly connected$ = this._connected$.pipe(distinctUntilChanged());
+  private readonly status$ = new ReplaySubject<NetworkStatus>(1);
+
+  readonly connected$ = merge(
+    defer(() => this.networkPlugin.getStatus()),
+    this.status$
+  ).pipe(pluck('connected'), distinctUntilChanged());
 
   constructor(
     @Inject(NETOWRK_PLUGIN)
     private readonly networkPlugin: NetworkPlugin,
-    private readonly ngZone: NgZone
-  ) {}
-
-  async initialize() {
-    const currentStatus = await this.networkPlugin.getStatus();
-    this.updateNetworkStatus(currentStatus);
+    private readonly zone: NgZone
+  ) {
     this.networkPlugin.addListener('networkStatusChange', status => {
-      this.ngZone.run(() => this.updateNetworkStatus(status));
+      this.zone.run(() => this.status$.next(status));
     });
-  }
-
-  updateNetworkStatus(status: NetworkStatus) {
-    this._connected$.next(status.connected);
   }
 }
