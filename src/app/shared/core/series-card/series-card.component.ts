d229a04be0994dbc10fe64482226e8cd2e29a6a7
DaYuan <sduum82@gmail.com>
2021-03-31 19:31:31 +0800

    fix css to new design

--- src/app/shared/core/series-card/series-card.component.ts
+++ src/app/shared/core/series-card/series-card.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-series-card',
   templateUrl: './series-card.component.html',
   styleUrls: ['./series-card.component.scss'],
 })
-export class SeriesCardComponent {}
+export class SeriesCardComponent {
+  @Input() src: any;
+}
