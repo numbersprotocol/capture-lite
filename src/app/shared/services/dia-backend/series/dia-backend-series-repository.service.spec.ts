74fee154641e3fbcd6055fe15199ca9a8259e7d2
DaYuan <sduum82@gmail.com>
2021-03-31 19:34:55 +0800

    add Dia Backend Series service

---
+++ src/app/shared/services/dia-backend/series/dia-backend-series-repository.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { SharedTestingModule } from '../../../shared-testing.module';
+import { DiaBackendSeriesRepository } from './dia-backend-series-repository.service';
+
+describe('DiaBackendSeriesRepository', () => {
+  let service: DiaBackendSeriesRepository;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SharedTestingModule],
+    });
+    service = TestBed.inject(DiaBackendSeriesRepository);
+  });
+
+  it('should be created', () => expect(service).toBeTruthy());
+});
---
+++ src/app/shared/services/dia-backend/series/dia-backend-series-repository.service.ts
@@ -0,0 +1,52 @@
+// tslint:disable: no-magic-numbers
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { defer, Subject, throwError } from 'rxjs';
+import { catchError, concatMap } from 'rxjs/operators';
+import { Tuple } from '../../database/table/table';
+import { DiaBackendAuthService } from '../auth/dia-backend-auth.service';
+import { NotFoundErrorResponse } from '../errors';
+import { BASE_URL } from '../secret';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DiaBackendSeriesRepository {
+  private readonly postCapturesUpdated$ = new Subject<{ reason?: string }>();
+
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly authService: DiaBackendAuthService
+  ) {}
+
+  readSeries$() {
+    return defer(() => this.authService.getAuthHeaders()).pipe(
+      concatMap(headers =>
+        this.httpClient.get<DiaBackendSeries>(`${BASE_URL}/api/v2/series/`, {
+          headers,
+        })
+      ),
+      catchError(err => {
+        if (err instanceof HttpErrorResponse && err.status === 404) {
+          return throwError(new NotFoundErrorResponse(err));
+        }
+        return throwError(err);
+      })
+    );
+  }
+}
+
+export interface DiaBackendSeries extends Tuple {
+  readonly id: string;
+  readonly collections: string;
+  readonly in_store: boolean | null;
+  readonly owner: string;
+  readonly owner_name: string;
+  readonly type: string;
+  readonly cover_image: string;
+  readonly title: string;
+  readonly collected: boolean;
+  readonly description: string;
+  readonly created_at: string;
+  readonly updated_at: string;
+}
