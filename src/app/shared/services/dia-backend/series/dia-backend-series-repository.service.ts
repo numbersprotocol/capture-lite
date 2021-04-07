014d1c85344fd54e17b9b60dd05fea1cd0188316
Sean Wu <seanwu1105@gmail.com>
2021-04-07 21:45:49 +0800

    Connect with series APIs. (#618)
    * Connect with series APIs.
    
    * Add series cover placeholder.
    
    * Merge /icon and /image folders into /images.
    
    * Adjust tab button position.
    
    * Show collected series only.

--- src/app/shared/services/dia-backend/series/dia-backend-series-repository.service.ts
+++ src/app/shared/services/dia-backend/series/dia-backend-series-repository.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { defer, throwError } from 'rxjs';
-import { catchError, concatMap } from 'rxjs/operators';
+import { catchError, concatMap, pluck, switchMap } from 'rxjs/operators';
 import { DiaBackendAuthService } from '../auth/dia-backend-auth.service';
 import { NotFoundErrorResponse } from '../errors';
 import { PaginatedResponse } from '../pagination';
@@ -11,33 +15,76 @@ import { BASE_URL } from '../secret';
   providedIn: 'root',
 })
 export class DiaBackendSeriesRepository {
-  readonly fetchSeries$ = defer(() => this.authService.getAuthHeaders()).pipe(
-    concatMap(headers =>
-      this.httpClient.get<PaginatedResponse<DiaBackendSeries>>(
-        `${BASE_URL}/api/v2/series/`,
-        {
-          headers,
-        }
-      )
-    ),
-    catchError((err: unknown) => {
-      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-      if (err instanceof HttpErrorResponse && err.status === 404) {
-        return throwError(new NotFoundErrorResponse(err));
-      }
-      return throwError(err);
-    })
+  private readonly fetchCollectedSeriesCount$ = this.fetch$({
+    limit: 1,
+    collected: true,
+  }).pipe(pluck('count'));
+
+  readonly fetchCollectedSeriesList$ = this.fetchCollectedSeriesCount$.pipe(
+    switchMap(count => this.fetch$({ limit: count, collected: true }))
   );
 
   constructor(
     private readonly httpClient: HttpClient,
     private readonly authService: DiaBackendAuthService
   ) {}
+
+  fetchById$(id: string) {
+    return this.read$(id);
+  }
+
+  private fetch$({
+    limit,
+    collected,
+  }: {
+    limit?: number;
+    collected?: boolean;
+  }) {
+    return defer(() => this.authService.getAuthHeaders()).pipe(
+      concatMap(headers => {
+        let params = new HttpParams();
+
+        if (limit !== undefined) {
+          params = params.set('limit', `${limit}`);
+        }
+        if (collected !== undefined) {
+          params = params.set('collected', `${collected}`);
+        }
+        return this.httpClient.get<PaginatedResponse<DiaBackendSeries>>(
+          `${BASE_URL}/api/v2/series/`,
+          { headers, params }
+        );
+      })
+    );
+  }
+
+  private read$(id: string) {
+    return defer(() => this.authService.getAuthHeaders()).pipe(
+      concatMap(headers =>
+        this.httpClient.get<DiaBackendSeries>(
+          `${BASE_URL}/api/v2/series/${id}/`,
+          { headers }
+        )
+      ),
+      catchError((err: unknown) => {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        if (err instanceof HttpErrorResponse && err.status === 404)
+          return throwError(new NotFoundErrorResponse(err));
+        return throwError(err);
+      })
+    );
+  }
 }
 
 export interface DiaBackendSeries {
   readonly id: string;
-  readonly collections: string;
+  readonly collections: {
+    readonly assets: {
+      readonly id: string;
+      readonly sharable_copy: string;
+      readonly collected: boolean;
+    }[];
+  }[];
   readonly in_store: boolean | null;
   readonly cover_image: string | null;
 }
