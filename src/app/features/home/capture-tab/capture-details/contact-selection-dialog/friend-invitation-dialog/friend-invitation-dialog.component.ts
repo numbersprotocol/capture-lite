72337aa05184e95a0fcdf80ea4b70ab4dada4754
Sean Wu <seanwu1105@gmail.com>
2021-04-22 15:10:41 +0800

    Handle Android back button on a dialog opened.

--- src/app/features/home/capture-tab/capture-details/contact-selection-dialog/friend-invitation-dialog/friend-invitation-dialog.component.ts
+++ src/app/features/home/capture-tab/capture-details/contact-selection-dialog/friend-invitation-dialog/friend-invitation-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { AndroidBackButtonService } from '../../../../../../shared/services/android-back-button/android-back-button.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-friend-invitation-dialog',
   templateUrl: './friend-invitation-dialog.component.html',
@@ -9,8 +12,14 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class FriendInvitationDialogComponent {
   constructor(
     private readonly dialogRef: MatDialogRef<FriendInvitationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: SubmittedFriendInvitation
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: SubmittedFriendInvitation,
+    private readonly androidBackButtonService: AndroidBackButtonService
+  ) {
+    this.androidBackButtonService
+      .closeMatDialog$(dialogRef)
+      .pipe(untilDestroyed(this))
+      .subscribe();
+  }
 
   onCancelClicked() {
     this.dialogRef.close();
