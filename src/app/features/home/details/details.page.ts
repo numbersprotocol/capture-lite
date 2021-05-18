412a3ac55ac349c08415e9d7761f3cd118b937f0
Sean Wu <seanwu1105@gmail.com>
2021-05-18 16:11:19 +0800

    Implement DetailedCapture class.

--- src/app/features/home/details/details.page.ts
+++ src/app/features/home/details/details.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { TranslocoService } from '@ngneat/transloco';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { defer, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { defer, Observable, of } from 'rxjs';
+import { catchError, first, map } from 'rxjs/operators';
 import SwiperCore, { Virtual } from 'swiper/core';
-import { DiaBackendAsset } from '../../../shared/dia-backend/asset/dia-backend-asset-repository.service';
+import {
+  DiaBackendAsset,
+  DiaBackendAssetRepository,
+} from '../../../shared/dia-backend/asset/dia-backend-asset-repository.service';
+import { DiaBackendAuthService } from '../../../shared/dia-backend/auth/dia-backend-auth.service';
+import { ErrorService } from '../../../shared/error/error.service';
+import { MediaStore } from '../../../shared/media/media-store/media-store.service';
+import { OldDefaultInformationName } from '../../../shared/repositories/proof/old-proof-adapter';
 import { Proof } from '../../../shared/repositories/proof/proof';
 import { ProofRepository } from '../../../shared/repositories/proof/proof-repository.service';
+import { blobToBase64 } from '../../../utils/encoding/encoding';
 
 SwiperCore.use([Virtual]);
 
@@ -17,15 +26,34 @@ SwiperCore.use([Virtual]);
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage {
-  private readonly detailedCaptures$: Observable<
+  readonly detailedCaptures$: Observable<
     DetailedCapture[]
   > = this.proofRepository.all$.pipe(
-    map(proofs => proofs.map(p => new DetailedCapture(p)))
+    map(proofs =>
+      proofs
+        .sort((a, b) => b.timestamp - a.timestamp)
+        .map(
+          p =>
+            new DetailedCapture(
+              p,
+              this.mediaStore,
+              this.diaBackendAssetRepository,
+              this.errorService,
+              this.diaBackendAuthService,
+              this.translocoService
+            )
+        )
+    )
   );
 
   constructor(
     private readonly navController: NavController,
-    private readonly proofRepository: ProofRepository
+    private readonly proofRepository: ProofRepository,
+    private readonly mediaStore: MediaStore,
+    private readonly diaBackendAssetRepository: DiaBackendAssetRepository,
+    private readonly errorService: ErrorService,
+    private readonly diaBackendAuthService: DiaBackendAuthService,
+    private readonly translocoService: TranslocoService
   ) {}
 
   navigateBack() {
@@ -35,8 +63,21 @@ export class DetailsPage {
 
 class DetailedCapture {
   readonly mediaUrl$ = defer(async () => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return this.proofOrDiaBackendAsset.getFirstAssetUrl();
+    if (this.proofOrDiaBackendAsset instanceof Proof) {
+      const proof = this.proofOrDiaBackendAsset;
+      const [index, meta] = Object.entries(proof.indexedAssets)[0];
+      if (!(await this.mediaStore.exists(index)) && proof.diaBackendAssetId) {
+        const mediaBlob = await this.diaBackendAssetRepository
+          .downloadFile$({ id: proof.diaBackendAssetId, field: 'asset_file' })
+          .pipe(
+            first(),
+            catchError((err: unknown) => this.errorService.toastError$(err))
+          )
+          .toPromise();
+        await proof.setAssets({ [await blobToBase64(mediaBlob)]: meta });
+      }
+      return proof.getFirstAssetUrl();
+    }
 
     return this.proofOrDiaBackendAsset.asset_file;
   });
@@ -47,7 +88,79 @@ class DetailedCapture {
     return this.proofOrDiaBackendAsset.asset_file_mime_type;
   });
 
+  readonly creator$ = defer(() => {
+    if (this.proofOrDiaBackendAsset instanceof Proof)
+      return this.diaBackendAuthService.username$;
+    return of(this.proofOrDiaBackendAsset.creator_name);
+  });
+
+  readonly location$ = defer(async () => {
+    const fixedLength = 6;
+    let geolocation: { latitude: number; longitude: number } | undefined;
+    if (this.proofOrDiaBackendAsset instanceof Proof) {
+      geolocation = normalizeGeolocation({
+        latitude: this.proofOrDiaBackendAsset.geolocationLatitude,
+        longitude: this.proofOrDiaBackendAsset.geolocationLongitude,
+      });
+    } else {
+      geolocation = getGeolocation(this.proofOrDiaBackendAsset);
+    }
+    if (geolocation)
+      return `${geolocation.latitude.toFixed(
+        fixedLength
+      )}, ${geolocation.longitude.toFixed(fixedLength)}`;
+    return this.translocoService.translate<string>('locationNotProvided');
+  });
+
+  readonly timestamp$ = defer(async () => {
+    if (this.proofOrDiaBackendAsset instanceof Proof)
+      return this.proofOrDiaBackendAsset.timestamp;
+    return this.proofOrDiaBackendAsset.information.proof?.timestamp;
+  });
+
+  readonly mediaId$ = defer(async () => {
+    if (this.proofOrDiaBackendAsset instanceof Proof)
+      return this.proofOrDiaBackendAsset.diaBackendAssetId;
+    return this.proofOrDiaBackendAsset.id;
+  });
+
   constructor(
-    private readonly proofOrDiaBackendAsset: Proof | DiaBackendAsset
+    private readonly proofOrDiaBackendAsset: Proof | DiaBackendAsset,
+    private readonly mediaStore: MediaStore,
+    private readonly diaBackendAssetRepository: DiaBackendAssetRepository,
+    private readonly errorService: ErrorService,
+    private readonly diaBackendAuthService: DiaBackendAuthService,
+    private readonly translocoService: TranslocoService
   ) {}
+}
+
+function getGeolocation(diaBackendAsset: DiaBackendAsset) {
+  const latitude = diaBackendAsset.information.information?.find(
+    info => info.name === OldDefaultInformationName.GEOLOCATION_LATITUDE
+  )?.value;
+  const longitude = diaBackendAsset.information.information?.find(
+    info => info.name === OldDefaultInformationName.GEOLOCATION_LONGITUDE
+  )?.value;
+  return normalizeGeolocation({ latitude, longitude });
+}
+
+function normalizeGeolocation({
+  latitude,
+  longitude,
+}: {
+  latitude: any;
+  longitude: any;
+}) {
+  if (
+    ((typeof latitude === 'string' &&
+      latitude !== 'undefined' &&
+      latitude !== '') ||
+      typeof latitude === 'number') &&
+    ((typeof longitude === 'string' &&
+      longitude !== 'undefined' &&
+      longitude !== '') ||
+      typeof longitude === 'number')
+  )
+    return { latitude: Number(latitude), longitude: Number(longitude) };
+  return undefined;
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,3 +63,5 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'zone.js/dist/zone-patch-rxjs';
