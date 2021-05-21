e7b61de70c8dc151eb7019909d90b3a9a3de8429
Sean Wu <seanwu1105@gmail.com>
2021-05-21 12:06:02 +0800

    Implement information page for all details usage.

--- src/app/features/home/details/details.page.ts
+++ src/app/features/home/details/details.page.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Plugins } from '@capacitor/core';
-import { ActionSheetController, NavController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
 import { ActionSheetButton } from '@ionic/core';
 import { TranslocoService } from '@ngneat/transloco';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import {
-  combineLatest,
-  defer,
-  EMPTY,
-  Observable,
-  of,
-  ReplaySubject,
-} from 'rxjs';
+import { combineLatest, defer, EMPTY, Observable, ReplaySubject } from 'rxjs';
 import {
   catchError,
   concatMap,
@@ -22,7 +15,6 @@ import {
   first,
   map,
   pluck,
-  shareReplay,
   switchMap,
   tap,
 } from 'rxjs/operators';
@@ -30,26 +22,21 @@ import SwiperCore, { Swiper, Virtual } from 'swiper/core';
 import { BlockingActionService } from '../../../shared/blocking-action/blocking-action.service';
 import { ConfirmAlert } from '../../../shared/confirm-alert/confirm-alert.service';
 import { ContactSelectionDialogComponent } from '../../../shared/contact-selection-dialog/contact-selection-dialog.component';
-import {
-  DiaBackendAsset,
-  DiaBackendAssetRepository,
-} from '../../../shared/dia-backend/asset/dia-backend-asset-repository.service';
+import { DiaBackendAssetRepository } from '../../../shared/dia-backend/asset/dia-backend-asset-repository.service';
 import { DiaBackendAuthService } from '../../../shared/dia-backend/auth/dia-backend-auth.service';
 import { ErrorService } from '../../../shared/error/error.service';
 import { MediaStore } from '../../../shared/media/media-store/media-store.service';
-import {
-  getOldProof,
-  OldDefaultInformationName,
-} from '../../../shared/repositories/proof/old-proof-adapter';
-import { Proof } from '../../../shared/repositories/proof/proof';
 import { ProofRepository } from '../../../shared/repositories/proof/proof-repository.service';
 import { ShareService } from '../../../shared/share/share.service';
-import { blobToBase64 } from '../../../utils/encoding/encoding';
 import {
   isNonNullable,
   switchTap,
   VOID$,
 } from '../../../utils/rx-operators/rx-operators';
+import {
+  DetailedCapture,
+  InformationSessionService,
+} from './information/session/information-session.service';
 
 SwiperCore.use([Virtual]);
 
@@ -173,13 +160,16 @@ export class DetailsPage {
   );
 
   readonly activeDetailedCapture$ = this._activeDetailedCapture$.pipe(
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    tap(
+      detailedCapture =>
+        (this.informationSessionService.activatedDetailedCapture = detailedCapture)
+    )
   );
 
   readonly isFromSeriesPage$ = this.type$.pipe(map(type => type === 'series'));
 
   constructor(
-    private readonly navController: NavController,
     private readonly proofRepository: ProofRepository,
     private readonly mediaStore: MediaStore,
     private readonly diaBackendAssetRepository: DiaBackendAssetRepository,
@@ -192,15 +182,12 @@ export class DetailsPage {
     private readonly actionSheetController: ActionSheetController,
     private readonly shareService: ShareService,
     private readonly confirmAlert: ConfirmAlert,
-    private readonly blockingActionService: BlockingActionService
+    private readonly blockingActionService: BlockingActionService,
+    private readonly informationSessionService: InformationSessionService
   ) {
     this.initializeActiveDetailedCapture$
       .pipe(untilDestroyed(this))
       .subscribe();
-  }
-
-  navigateBack() {
-    return this.navController.back();
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -459,145 +446,4 @@ export class DetailsPage {
       )
       .subscribe();
   }
-}
-
-class DetailedCapture {
-  readonly id =
-    this.proofOrDiaBackendAsset instanceof Proof
-      ? this.proofOrDiaBackendAsset.diaBackendAssetId
-      : this.proofOrDiaBackendAsset.id;
-
-  readonly hash =
-    this.proofOrDiaBackendAsset instanceof Proof
-      ? getOldProof(this.proofOrDiaBackendAsset).hash
-      : this.proofOrDiaBackendAsset.proof_hash;
-
-  readonly mediaUrl$ = defer(() => {
-    if (this.proofOrDiaBackendAsset instanceof Proof) {
-      const proof = this.proofOrDiaBackendAsset;
-      return defer(async () => {
-        const [index, meta] = Object.entries(proof.indexedAssets)[0];
-        if (!(await this.mediaStore.exists(index)) && proof.diaBackendAssetId) {
-          const mediaBlob = await this.diaBackendAssetRepository
-            .downloadFile$({ id: proof.diaBackendAssetId, field: 'asset_file' })
-            .pipe(
-              first(),
-              catchError((err: unknown) => this.errorService.toastError$(err))
-            )
-            .toPromise();
-          await proof.setAssets({ [await blobToBase64(mediaBlob)]: meta });
-        }
-        return proof.getFirstAssetUrl();
-      });
-    }
-
-    return this.diaBackendAsset$.pipe(
-      isNonNullable(),
-      switchMap(asset =>
-        this.diaBackendAssetRepository.getAndCachePostCaptureMedia$(asset)
-      ),
-      catchError((err: unknown) => this.errorService.toastError$(err)),
-      first(),
-      map(blob => URL.createObjectURL(blob)),
-      shareReplay({ bufferSize: 1, refCount: true })
-    );
-  });
-
-  readonly mediaMimeType$ = defer(async () => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return (await this.proofOrDiaBackendAsset.getFirstAssetMeta()).mimeType;
-    return this.proofOrDiaBackendAsset.asset_file_mime_type;
-  });
-
-  readonly creator$ = defer(() => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return this.diaBackendAuthService.username$;
-    return of(this.proofOrDiaBackendAsset.creator_name);
-  });
-
-  readonly geolocation$ = defer(async () => {
-    if (this.proofOrDiaBackendAsset instanceof Proof) {
-      return normalizeGeolocation({
-        latitude: this.proofOrDiaBackendAsset.geolocationLatitude,
-        longitude: this.proofOrDiaBackendAsset.geolocationLongitude,
-      });
-    }
-    return getGeolocation(this.proofOrDiaBackendAsset);
-  });
-
-  readonly locationDisplay$ = this.geolocation$.pipe(
-    map(geolocation => {
-      const fixedLength = 6;
-      if (geolocation)
-        return `${geolocation.latitude.toFixed(
-          fixedLength
-        )}, ${geolocation.longitude.toFixed(fixedLength)}`;
-      return this.translocoService.translate<string>('notDisclosed');
-    })
-  );
-
-  readonly timestamp$ = defer(async () => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return this.proofOrDiaBackendAsset.timestamp;
-    return this.proofOrDiaBackendAsset.information.proof?.timestamp;
-  });
-
-  readonly diaBackendAsset$ = defer(() => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return this.diaBackendAssetRepository
-        .fetchByProof$(this.proofOrDiaBackendAsset)
-        .pipe(catchError(() => of(undefined)));
-    return of(this.proofOrDiaBackendAsset);
-  });
-
-  readonly proof$ = defer(async () => {
-    if (this.proofOrDiaBackendAsset instanceof Proof)
-      return this.proofOrDiaBackendAsset;
-    return undefined;
-  });
-
-  readonly isFromStore =
-    this.proofOrDiaBackendAsset instanceof Proof
-      ? false
-      : this.proofOrDiaBackendAsset.source_type === 'store';
-
-  constructor(
-    private readonly proofOrDiaBackendAsset: Proof | DiaBackendAsset,
-    private readonly mediaStore: MediaStore,
-    private readonly diaBackendAssetRepository: DiaBackendAssetRepository,
-    private readonly errorService: ErrorService,
-    private readonly diaBackendAuthService: DiaBackendAuthService,
-    private readonly translocoService: TranslocoService
-  ) {}
-}
-
-function getGeolocation(diaBackendAsset: DiaBackendAsset) {
-  const latitude = diaBackendAsset.information.information?.find(
-    info => info.name === OldDefaultInformationName.GEOLOCATION_LATITUDE
-  )?.value;
-  const longitude = diaBackendAsset.information.information?.find(
-    info => info.name === OldDefaultInformationName.GEOLOCATION_LONGITUDE
-  )?.value;
-  return normalizeGeolocation({ latitude, longitude });
-}
-
-export function normalizeGeolocation({
-  latitude,
-  longitude,
-}: {
-  latitude: any;
-  longitude: any;
-}) {
-  if (
-    ((typeof latitude === 'string' &&
-      latitude !== 'undefined' &&
-      latitude !== '') ||
-      typeof latitude === 'number') &&
-    ((typeof longitude === 'string' &&
-      longitude !== 'undefined' &&
-      longitude !== '') ||
-      typeof longitude === 'number')
-  )
-    return { latitude: Number(latitude), longitude: Number(longitude) };
-  return undefined;
 }
---
+++ src/app/features/home/details/information/information.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../../../shared/shared.module';
+import { InformationPageRoutingModule } from './information-routing.module';
+import { InformationPage } from './information.page';
+
+@NgModule({
+  imports: [SharedModule, InformationPageRoutingModule],
+  declarations: [InformationPage],
+})
+export class InformationPageModule {}
