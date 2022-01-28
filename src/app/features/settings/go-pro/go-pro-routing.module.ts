fe1813a4e250efd11a251b094b2e7ae5461739d4
sultanmyrza@gmail.com <sultanmyrza@gmail.com>
2022-01-28 12:01:44 +0800

    remove unused GoPro UI components

--- src/app/features/settings/go-pro/go-pro-routing.module.ts
+++ src/app/features/settings/go-pro/go-pro-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GoProMediaItemDetailOnCameraComponent } from './go-pro-media-item-detail-on-camera/go-pro-media-item-detail-on-camera.component';
-import { GoProMediaItemDetailOnDeviceComponent } from './go-pro-media-item-detail-on-device/go-pro-media-item-detail-on-device.component';
 import { GoProMediaListOnCameraComponent } from './go-pro-media-list-on-camera/go-pro-media-list-on-camera.component';
-import { GoProMediaListOnDeviceComponent } from './go-pro-media-list-on-device/go-pro-media-list-on-device.component';
 import { GoProPage } from './go-pro.page';
 
 const routes: Routes = [
@@ -18,15 +16,6 @@ const routes: Routes = [
   {
     path: 'media-item-detail-on-camera',
     component: GoProMediaItemDetailOnCameraComponent,
-  },
-
-  {
-    path: 'media-list-on-device',
-    component: GoProMediaListOnDeviceComponent,
-  },
-  {
-    path: 'media-item-detail-on-device',
-    component: GoProMediaItemDetailOnDeviceComponent,
   },
 ];
 
--- src/app/features/settings/go-pro/go-pro.module.ts
+++ src/app/features/settings/go-pro/go-pro.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { GoProMediaItemDetailOnCameraComponent } from './go-pro-media-item-detail-on-camera/go-pro-media-item-detail-on-camera.component';
-import { GoProMediaItemDetailOnDeviceComponent } from './go-pro-media-item-detail-on-device/go-pro-media-item-detail-on-device.component';
 import { GoProMediaListItemOnCameraComponent } from './go-pro-media-list-item-on-camera/go-pro-media-list-item-on-camera.component';
-import { GoProMediaListItemOnDeviceComponent } from './go-pro-media-list-item-on-device/go-pro-media-list-item-on-device.component';
 import { GoProMediaListOnCameraComponent } from './go-pro-media-list-on-camera/go-pro-media-list-on-camera.component';
-import { GoProMediaListOnDeviceComponent } from './go-pro-media-list-on-device/go-pro-media-list-on-device.component';
 import { GoProPageRoutingModule } from './go-pro-routing.module';
 import { GoProPage } from './go-pro.page';
 
@@ -17,10 +14,6 @@ import { GoProPage } from './go-pro.page';
     GoProMediaListOnCameraComponent,
     GoProMediaListItemOnCameraComponent,
     GoProMediaItemDetailOnCameraComponent,
-
-    GoProMediaListOnDeviceComponent,
-    GoProMediaListItemOnDeviceComponent,
-    GoProMediaItemDetailOnDeviceComponent,
   ],
 })
 export class GoProPageModule {}
