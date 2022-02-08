ad62da78dce8049e036281cf7a5312c8872beb9e
Ray Hung <ruizehung@gatech.edu>
2022-02-08 22:35:45 +0800

    fix(dia-backend-asset-refreshing.service.ts): ignore refresh if there is pending upload task

--- src/app/shared/dia-backend/asset/refreshing/dia-backend-asset-refreshing.service.ts
+++ src/app/shared/dia-backend/asset/refreshing/dia-backend-asset-refreshing.service.ts
@@ -1,23 +1,37 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { EMPTY, forkJoin } from 'rxjs';
 import { catchError, concatMap, first } from 'rxjs/operators';
 import { HttpErrorCode } from '../../../error/error.service';
 import { ProofRepository } from '../../../repositories/proof/proof-repository.service';
 import { DiaBackendAssetRepository } from '../dia-backend-asset-repository.service';
 import { DiaBackendAssetPrefetchingService } from '../prefetching/dia-backend-asset-prefetching.service';
+import { DiaBackendAssetUploadingService } from '../uploading/dia-backend-asset-uploading.service';
 
+@UntilDestroy()
 @Injectable({
   providedIn: 'root',
 })
 export class DiaBackendAsseRefreshingService {
+  private pendingUploadTasks = 0;
+
   constructor(
     private readonly assetRepository: DiaBackendAssetRepository,
     private readonly proofRepository: ProofRepository,
-    private readonly diaBackendAssetPrefetchingService: DiaBackendAssetPrefetchingService
-  ) {}
+    private readonly diaBackendAssetPrefetchingService: DiaBackendAssetPrefetchingService,
+    private readonly uploadService: DiaBackendAssetUploadingService
+  ) {
+    this.uploadService.pendingTasks$
+      .pipe(untilDestroyed(this))
+      .subscribe(value => (this.pendingUploadTasks = value));
+  }
 
   refresh() {
+    // Don't refresh if there are still captures being uploaded.
+    if (this.pendingUploadTasks > 0) {
+      return EMPTY;
+    }
     return this.proofRepository.all$.pipe(
       first(),
       concatMap(proofs =>
