1688367f403514d557a618b10b7597981729bde0
sultanmyrza@gmail.com <sultanmyrza@gmail.com>
2022-02-12 12:48:18 +0800

    refactor(go-pro-wifi.service.ts): use preferenceManager instead of Plugins.Storage

--- src/app/features/settings/go-pro/services/go-pro-wifi.service.ts
+++ src/app/features/settings/go-pro/services/go-pro-wifi.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { WifiPlugin } from '@capacitor-community/wifi';
 import { Plugins } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
+import { PreferenceManager } from '../../../../shared/preference-manager/preference-manager.service';
 import { GoProBluetoothService } from './go-pro-bluetooth.service';
 const Wifi: WifiPlugin = Plugins.Wifi as WifiPlugin;
-
-const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoProWifiService {
-  private readonly GO_PRO_TUTORIAL_MOBILE_DATA_ONLY_APPS_STORAGE_KEY =
-    'GO_PRO_TUTORIAL_MOBILE_DATA_ONLY_APPS_STORAGE_KEY';
+  readonly id = 'GoProWifiService';
+
+  private readonly preferences = this.preferenceManager.getPreferences(this.id);
 
   constructor(
+    private readonly preferenceManager: PreferenceManager,
     private readonly goProBluetoothService: GoProBluetoothService,
     public platform: Platform
   ) {}
@@ -45,21 +46,22 @@ export class GoProWifiService {
   async showTutorialForMobileDataOnlyApps() {
     if (this.platform.is('android') === false) return false;
 
-    const result = await Storage.get({
-      key: this.GO_PRO_TUTORIAL_MOBILE_DATA_ONLY_APPS_STORAGE_KEY,
-    });
+    const result = await this.preferences.getBoolean(
+      PrefKeys.SHOW_MOBILE_DATA_TUTORIAL_ON_IOS,
+      true
+    );
 
-    if (result.value) {
-      return JSON.parse(result.value) as boolean;
-    }
-
-    return false;
+    return result;
   }
 
   async dontShowAgainTutorialForMobileDataOnlyApps() {
-    await Storage.set({
-      key: this.GO_PRO_TUTORIAL_MOBILE_DATA_ONLY_APPS_STORAGE_KEY,
-      value: JSON.stringify(true),
-    });
+    await this.preferences.setBoolean(
+      PrefKeys.SHOW_MOBILE_DATA_TUTORIAL_ON_IOS,
+      false
+    );
   }
+}
+
+const enum PrefKeys {
+  SHOW_MOBILE_DATA_TUTORIAL_ON_IOS = 'GO_PRO_SHOW_MOBILE_DATA_TUTORIAL_ON_IOS',
 }
