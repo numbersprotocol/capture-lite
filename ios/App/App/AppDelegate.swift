63621e42506a2b6a8e421ed1f765cd8b82e19175
sultanmyrza@gmail.com <sultanmyrza@gmail.com>
2022-02-12 14:58:43 +0800

    Switch from CAPBridge to ApplicationDelegateProxy in application events

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -40,14 +40,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     // Called when the app was launched with a url. Feel free to add additional processing here,
     // but if you want the App API to support tracking app url opens, make sure to keep this call
-    return CAPBridge.handleOpenUrl(url, options)
+    return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
   }
   
   func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
     // Called when the app was launched with an activity, including Universal Links.
     // Feel free to add additional processing here, but if you want the App API to support
     // tracking app url opens, make sure to keep this call
-    return CAPBridge.handleContinueActivity(userActivity, restorationHandler)
+    return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
