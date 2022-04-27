51002e4b16a324dba5e8259f6f28ce3359d725bc
sultanmyrza-numbersprotocol <sultanmyrza@numbersprotocol.io>
2022-04-27 20:28:37 +0800

    Revert "Revert "Merge pull request #1523 from sultanmyrza/feature-custom-camera-ux""
    This reverts commit 8fe89fb683488ea9968c170a0a6ede9b820d429f.

---
+++ src/app/features/home/custom-camera/custom-camera.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { NgxLongPress2Module } from 'ngx-long-press2';
+import { SharedModule } from '../../../shared/shared.module';
+import { CustomCameraPageRoutingModule } from './custom-camera-routing.module';
+import { CustomCameraPage } from './custom-camera.page';
+import { CustomCameraService } from './custom-camera.service';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CustomCameraPageRoutingModule,
+    NgxLongPress2Module,
+    NgCircleProgressModule.forRoot({}),
+  ],
+  providers: [CustomCameraService],
+  declarations: [CustomCameraPage],
+})
+export class CustomCameraPageModule {}
