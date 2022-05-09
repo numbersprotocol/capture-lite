3e9e1669dab02b6bf4df77cb27f1bbdea549a3a7
sultanmyrza-numbersprotocol <sultanmyrza@numbersprotocol.io>
2022-05-09 14:41:49 +0800

    feat(invitation.page): implement basic ui

--- src/app/features/invitation/invitation.module.ts
+++ src/app/features/invitation/invitation.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { SharedModule } from '../../shared/shared.module';
 import { InvitationPageRoutingModule } from './invitation-routing.module';
-
 import { InvitationPage } from './invitation.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    InvitationPageRoutingModule,
-  ],
+  imports: [SharedModule, InvitationPageRoutingModule],
   declarations: [InvitationPage],
 })
 export class InvitationPageModule {}
