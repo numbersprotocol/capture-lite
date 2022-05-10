d3aa29e25efe0b38c9778d93ee7fd2272ab00ee2
sultanmyrza-numbersprotocol <sultanmyrza@numbersprotocol.io>
2022-05-10 18:28:44 +0800

    fix(share.service.ts): get share text via translocoService

--- src/app/shared/share/share.service.ts
+++ src/app/shared/share/share.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Share } from '@capacitor/share';
+import { TranslocoService } from '@ngneat/transloco';
 import { catchError } from 'rxjs/operators';
 import { getAssetProfileUrl } from '../../utils/url';
 import {
@@ -16,7 +17,8 @@ export class ShareService {
 
   constructor(
     private readonly errorService: ErrorService,
-    private readonly diaBackendAssetRepository: DiaBackendAssetRepository
+    private readonly diaBackendAssetRepository: DiaBackendAssetRepository,
+    private readonly translocoService: TranslocoService
   ) {}
 
   async share(asset: DiaBackendAsset) {
@@ -36,11 +38,13 @@ export class ShareService {
     return getAssetProfileUrl(asset.id);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async shareReferralCode(referralCode: string) {
-    // TODO: use official (en, zh) text provided by @Tammy
+    const text = this.translocoService.translate(
+      'invitation.myReferralCodeAtCaptureAppIs',
+      { referralCode: referralCode }
+    );
     return Share.share({
-      text: `My referral code at Capture App ${referralCode}`,
+      text: text,
       url: 'https://www.numbersprotocol.io/#products',
     });
   }
