735e91b7d8063542f2f3598fa64c6c3706a837aa
sultanmyrza <sultanmyrza@gmail.com>
2022-06-06 20:50:06 +0800

    fix(share.service.ts): revert to previous functionality

--- src/app/features/home/details/details.page.ts
+++ src/app/features/home/details/details.page.ts
@@ -302,7 +302,7 @@ export class DetailsPage {
                     const result = await this.confirmAlert.present({
                       message:
                         this.translocoService.translate(
-                          'message.assetBecomePublicFor24Hours'
+                          'message.assetBecomePublicAfterSharing'
                         ) + '!',
                     });
                     if (result) {
