9eef8454b1af679fd2c2d4ec85de986f5803bd36
sultanmyrza <sultanmyrza@gmail.com>
2022-06-09 03:18:08 +0800

    add app-flyer service

---
+++ src/app/shared/app-flyer/app-flyer.service.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { Platform } from '@ionic/angular';
+import { SharedTestingModule } from '../shared-testing.module';
+import { AppFlyerService } from './app-flyer.service';
+
+describe('AppFlyerService', () => {
+  let service: AppFlyerService;
+  let platformIs: (platformName: string) => boolean;
+  let platformReadySpy: Promise<void>;
+  let platformSpy: Platform;
+
+  beforeEach(
+    waitForAsync(async () => {
+      platformReadySpy = Promise.resolve();
+      platformIs = _ => false;
+
+      platformSpy = jasmine.createSpyObj('Platform', {
+        ready: platformReadySpy,
+        is: platformIs,
+      });
+
+      await TestBed.configureTestingModule({
+        imports: [SharedTestingModule],
+        providers: [{ provide: Platform, useValue: platformSpy }],
+      }).compileComponents();
+
+      service = TestBed.inject(AppFlyerService);
+    })
+  );
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
