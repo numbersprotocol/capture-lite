77b828face85ac5bdf84385a742992e00aea0bb0
sultanmyrza <sultanmyrza@gmail.com>
2022-06-09 03:18:58 +0800

    init app flyer sdk on app start

--- set-secret.js
+++ set-secret.js
@@ -8,6 +8,7 @@ const envConfigFile = `
 export const BASE_URL = '${process.env.NUMBERS_STORAGE_BASE_URL}';
 export const TRUSTED_CLIENT_KEY = '${process.env.NUMBERS_STORAGE_TRUSTED_CLIENT_KEY}';
 export const BUBBLE_DB_URL = '${process.env.NUMBERS_BUBBLE_DB_URL}';
+export const APP_FLYER_DEV_KEY = '${process.env.APP_FLYER_DEV_KEY}'
 `;
 fs.writeFile(targetPath, envConfigFile, err => {
   if (err) {
