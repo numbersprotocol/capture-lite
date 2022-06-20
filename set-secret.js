f1d3c5583a19fe0cb9c1d19e064f4e4882fc335b
sultanmyrza <sultanmyrza@gmail.com>
2022-06-20 19:24:43 +0800

    add env var for BUBBLE_API endpoint

--- set-secret.js
+++ set-secret.js
@@ -8,6 +8,7 @@ const envConfigFile = `
 export const BASE_URL = '${process.env.NUMBERS_STORAGE_BASE_URL}';
 export const TRUSTED_CLIENT_KEY = '${process.env.NUMBERS_STORAGE_TRUSTED_CLIENT_KEY}';
 export const BUBBLE_DB_URL = '${process.env.NUMBERS_BUBBLE_DB_URL}';
+export const BUBBLE_API_URL = '${process.env.BUBBLE_API_URL}';
 `;
 fs.writeFile(targetPath, envConfigFile, err => {
   if (err) {
