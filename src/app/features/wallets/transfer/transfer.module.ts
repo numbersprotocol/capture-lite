cb8e31e9611df96d65c0dcc4f457a2ad734790ce
sultanmyrza <sultanmyrza@gmail.com>
2022-07-06 10:15:20 +0600

    move wallet address QR code to deposit page

--- src/app/features/wallets/transfer/transfer.module.ts
+++ src/app/features/wallets/transfer/transfer.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 import { SharedModule } from '../../../shared/shared.module';
 import { TransferLoadingComponent } from './transfer-loading/transfer-loading.component';
 import { TransferRequestSentComponent } from './transfer-request-sent/transfer-request-sent.component';
@@ -6,7 +7,7 @@ import { TransferPageRoutingModule } from './transfer-routing.module';
 import { TransferPage } from './transfer.page';
 
 @NgModule({
-  imports: [SharedModule, TransferPageRoutingModule],
+  imports: [SharedModule, TransferPageRoutingModule, NgxQRCodeModule],
   declarations: [
     TransferPage,
     TransferLoadingComponent,
