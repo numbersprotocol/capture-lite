6178fbcc3bcbe143c967fc6d7747435a28689d46
sultanmyrza <sultanmyrza@gmail.com>
2022-07-26 18:27:42 +0600

    feat: add explore tab with embedded test iframe

---
+++ src/app/features/home/explore-tab/explore-tab/explore-tab.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ErrorService } from '../../../../shared/error/error.service';
+import { NetworkService } from '../../../../shared/network/network.service';
+
+@Component({
+  selector: 'app-explore-tab',
+  templateUrl: './explore-tab.component.html',
+  styleUrls: ['./explore-tab.component.scss'],
+})
+export class ExploreTabComponent {
+  // TODO: change url to point to production bubble app
+  readonly bubbleIframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+    'https://test-iframe-sample.bubbleapps.io/version-test/'
+  );
+
+  readonly networkConnected$ = this.networkService.connected$;
+
+  constructor(
+    private readonly sanitizer: DomSanitizer,
+    private readonly networkService: NetworkService,
+    private readonly errorService: ErrorService
+  ) {}
+}
