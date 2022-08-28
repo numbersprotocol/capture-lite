00bdb559dede9ae981e10a4487453b0d24d42e1f
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2022-08-28 17:12:22 +0800

    build: add iframe url to set-secret.js
    To make iframe url configurable in the future
    
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- set-secret.js
+++ set-secret.js
@@ -8,6 +8,7 @@ const envConfigFile = `
 export const BASE_URL = '${process.env.NUMBERS_STORAGE_BASE_URL}';
 export const TRUSTED_CLIENT_KEY = '${process.env.NUMBERS_STORAGE_TRUSTED_CLIENT_KEY}';
 export const BUBBLE_DB_URL = '${process.env.NUMBERS_BUBBLE_DB_URL}';
+export const BUBBLE_IFRAME_URL = '${process.env.NUMBERS_BUBBLE_IFRAME_URL}';
 export const BUBBLE_API_URL = '${process.env.BUBBLE_API_URL}';
 export const APPS_FLYER_DEV_KEY = '${process.env.APPS_FLYER_DEV_KEY}'
 `;
