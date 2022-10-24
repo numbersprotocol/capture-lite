e69e05d3b323054b33abc9850f7ea95324e1a89f
sultanmyrza <sultanmyrza@gmail.com>
2022-10-24 16:39:32 +0800

    use pintura image editor for photos before publish

--- src/app/features/home/custom-camera/pre-publish-mode/pre-publish-mode.component.ts
+++ src/app/features/home/custom-camera/pre-publish-mode/pre-publish-mode.component.ts
@@ -1,23 +1,107 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  Output,
+} from '@angular/core';
+import { FilesystemPlugin } from '@capacitor/filesystem';
+import { getEditorDefaults } from '@pqina/pintura';
+import { BehaviorSubject, combineLatest, EMPTY, of, ReplaySubject } from 'rxjs';
+import { catchError, filter, first, map, switchMap, tap } from 'rxjs/operators';
+import { FILESYSTEM_PLUGIN } from '../../../../shared/capacitor-plugins/capacitor-plugins.module';
+import { blobToBase64 } from '../../../../utils/encoding/encoding';
+
+type CaptureMimeType = 'image/jpeg' | 'video/mp4';
 
 @Component({
   selector: 'app-pre-publish-mode',
   templateUrl: './pre-publish-mode.component.html',
   styleUrls: ['./pre-publish-mode.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PrePublishModeComponent {
-  @Input()
-  curCaptureFilePath?: string;
+  readonly pinturaEditorOptions: any = { ...getEditorDefaults() };
+
+  readonly curCaptureFilePath$ = new ReplaySubject<string>(1);
+
+  readonly curCaptureMimeType$ = new ReplaySubject<CaptureMimeType>(1);
+
+  readonly curCaptureSrc$ = new ReplaySubject<string>(1);
+
+  readonly isProcessingImage$ = new BehaviorSubject<boolean>(false);
+
+  readonly isVideo$ = this.curCaptureMimeType$.pipe(
+    map(mimeType => mimeType === 'video/mp4')
+  );
+
+  readonly isImage$ = this.curCaptureMimeType$.pipe(
+    map(mimeType => mimeType === 'image/jpeg')
+  );
+
+  readonly curImageBase64$ = combineLatest([
+    this.isImage$,
+    this.curCaptureFilePath$,
+  ]).pipe(
+    filter(([isImage, _]) => isImage),
+    switchMap(([_, path]) => this.filesystemPlugin.readFile({ path })),
+    map(result => `data:image/jpeg;base64,${result.data}`)
+  );
 
   @Input()
-  curCaptureMimeType?: 'image/jpeg' | 'video/mp4';
+  set curCaptureFilePath(value: string | undefined) {
+    if (value) this.curCaptureFilePath$.next(value);
+  }
 
   @Input()
-  curCaptureSrc?: string;
+  set curCaptureMimeType(value: CaptureMimeType | undefined) {
+    if (value) this.curCaptureMimeType$.next(value);
+  }
+
+  @Input()
+  set curCaptureSrc(value: string | undefined) {
+    if (value) this.curCaptureSrc$.next(value);
+  }
 
   @Output() discard: EventEmitter<any> = new EventEmitter();
 
   @Output() confirm: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    @Inject(FILESYSTEM_PLUGIN)
+    private readonly filesystemPlugin: FilesystemPlugin
+  ) {}
+
+  handleEditorProcessStart() {
+    this.isProcessingImage$.next(true);
+  }
+
+  handleEditorProcessAbort() {
+    this.isProcessingImage$.next(false);
+  }
+
+  handelEditorProcessError() {
+    this.isProcessingImage$.next(false);
+  }
+
+  async handleEditorProcess(imageWriterResult: any): Promise<void> {
+    const base64 = await blobToBase64(imageWriterResult.dest as File);
+
+    combineLatest([this.curCaptureFilePath$, of(base64)])
+      .pipe(
+        first(),
+        switchMap(([path, data]) =>
+          this.filesystemPlugin.writeFile({ path, data })
+        ),
+        tap(() => this.isProcessingImage$.next(false)),
+        catchError(() => {
+          this.isProcessingImage$.next(false);
+          return EMPTY;
+        })
+      )
+      .subscribe();
+  }
 
   onDiscard() {
     this.discard.emit(null);
