acef910a57e04316ebaec0b39aa29a0f2442be87
sultanmyrza <sultanmyrza@gmail.com>
2022-11-11 14:07:30 +0800

    listen for iframe reload events in details page

--- src/app/shared/iframe/iframe.service.ts
+++ src/app/shared/iframe/iframe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,18 @@ import { BehaviorSubject } from 'rxjs';
 export class IframeService {
   readonly exploreTabRefreshRequested$ = new BehaviorSubject(new Date());
 
+  private readonly _detailsPageIframeReloadRequested$ = new BehaviorSubject(
+    new Date()
+  );
+
+  readonly detailsPageIframeReloadRequested$ =
+    this._detailsPageIframeReloadRequested$.pipe(startWith(false));
+
   refreshExploreTabIframe() {
     this.exploreTabRefreshRequested$.next(new Date());
+  }
+
+  refreshDetailsPageIframe() {
+    this._detailsPageIframeReloadRequested$.next(new Date());
   }
 }
