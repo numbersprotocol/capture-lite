8aabdd9f700d46fb0f8ef19c90e913c8925dca51
sultanmyrza <sultanmyrza@gmail.com>
2023-01-10 21:41:27 +0800

    feat(menu): add Data policy menu item + page

---
+++ src/app/features/data-policy/data-policy.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { BUBBLE_IFRAME_URL } from '../../shared/dia-backend/secret';
+import { NetworkService } from '../../shared/network/network.service';
+
+@UntilDestroy()
+@Component({
+  selector: 'app-data-policy',
+  templateUrl: './data-policy.page.html',
+  styleUrls: ['./data-policy.page.scss'],
+})
+export class DataPolicyPage {
+  readonly networkConnected$ = this.networkService.connected$;
+  readonly dataPolicyUrl = `${BUBBLE_IFRAME_URL}/version-qa-release/data_policy`;
+  readonly iframeLoaded$ = new BehaviorSubject(false);
+
+  constructor(
+    private readonly networkService: NetworkService,
+    private readonly navController: NavController
+  ) {}
+
+  ionViewDidEnter() {
+    this.processIframeEvents();
+  }
+
+  processIframeEvents() {
+    fromEvent(window, 'message')
+      .pipe(
+        tap(event => {
+          const postMessageEvent = event as MessageEvent;
+          if (postMessageEvent.data === 'iframe-on-load') {
+            this.iframeLoaded$.next(true);
+          }
+          if (postMessageEvent.data === 'iframeBackButtonClicked') {
+            this.navController.back();
+          }
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe();
+  }
+}
