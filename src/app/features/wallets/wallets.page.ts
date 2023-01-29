2879fa1d6411b67c14f45c06e77a9a43261c412e
sultanmyrza <sultanmyrza@gmail.com>
2023-01-29 12:12:49 +0800

    fix(wallet.page): remove unused vars, methods

--- src/app/features/wallets/wallets.page.ts
+++ src/app/features/wallets/wallets.page.ts
@@ -1,32 +1,17 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatIconRegistry } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { Browser } from '@capacitor/browser';
 import { Clipboard } from '@capacitor/clipboard';
-import { NavController, Platform } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { TranslocoService } from '@ngneat/transloco';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NgxQrcodeElementTypes } from '@techiediaries/ngx-qrcode';
-import { BehaviorSubject, combineLatest, forkJoin, fromEvent } from 'rxjs';
-import {
-  catchError,
-  concatMap,
-  finalize,
-  first,
-  map,
-  switchMap,
-  tap,
-} from 'rxjs/operators';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+import { concatMap, first, map, tap } from 'rxjs/operators';
 import { WebCryptoApiSignatureProvider } from '../../shared/collector/signature/web-crypto-api-signature-provider/web-crypto-api-signature-provider.service';
-import { ConfirmAlert } from '../../shared/confirm-alert/confirm-alert.service';
 import { DiaBackendAuthService } from '../../shared/dia-backend/auth/dia-backend-auth.service';
 import { BUBBLE_IFRAME_URL } from '../../shared/dia-backend/secret';
 import { DiaBackendWalletService } from '../../shared/dia-backend/wallet/dia-backend-wallet.service';
-import { ErrorService } from '../../shared/error/error.service';
-import { ExportPrivateKeyModalComponent } from '../../shared/export-private-key-modal/export-private-key-modal.component';
 @UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-wallets',
@@ -34,19 +19,10 @@ import { ExportPrivateKeyModalComponent } from '../../shared/export-private-key-
   styleUrls: ['./wallets.page.scss'],
 })
 export class WalletsPage {
-  readonly domainMainnetNum = 'mainnet.num.network';
-
-  readonly mainNumBalance$ =
-    this.diaBackendWalletService.assetWalletMainnetNumBalance$;
-  readonly points$ = this.diaBackendAuthService.points$;
-
-  readonly totalBalance$ = new BehaviorSubject<number>(0);
-
   readonly publicKey$ = this.webCryptoApiSignatureProvider.publicKey$;
   readonly privateKey$ = this.webCryptoApiSignatureProvider.privateKey$;
   readonly assetWalletAddr$ = this.diaBackendWalletService.assetWalletAddr$;
 
-  readonly isLoadingBalance$ = new BehaviorSubject<boolean>(false);
   readonly networkConnected$ = this.diaBackendWalletService.networkConnected$;
 
   readonly iframeUrl$ = this.diaBackendAuthService.cachedQueryJWTToken$.pipe(
@@ -64,37 +40,12 @@ export class WalletsPage {
   constructor(
     private readonly diaBackendWalletService: DiaBackendWalletService,
     private readonly diaBackendAuthService: DiaBackendAuthService,
-    private readonly matIconRegistry: MatIconRegistry,
-    private readonly domSanitizer: DomSanitizer,
     private readonly snackBar: MatSnackBar,
     private readonly translocoService: TranslocoService,
     private readonly webCryptoApiSignatureProvider: WebCryptoApiSignatureProvider,
-    private readonly confirmAlert: ConfirmAlert,
-    private readonly dialog: MatDialog,
-    private readonly errorService: ErrorService,
     private readonly router: Router,
-    private readonly platform: Platform,
     private readonly navController: NavController
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'wallet',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../../../assets/images/wallet.svg'
-      )
-    );
-
-    combineLatest([this.mainNumBalance$, this.points$])
-      .pipe(
-        first(),
-        map(
-          ([mainNumBalance, points]) => Number(mainNumBalance) + Number(points)
-        ),
-        untilDestroyed(this)
-      )
-      .subscribe(totalBalance => this.totalBalance$.next(totalBalance));
-
-    this.shouldHideDepositButton = this.platform.is('ios');
-  }
+  ) {}
 
   ionViewDidEnter() {
     this.processIframeEvents();
@@ -163,87 +114,8 @@ export class WalletsPage {
       .subscribe();
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  onBuyNumBtnClicked() {
-    Browser.open({
-      url: `https://link.numbersprotocol.io/buy-num`,
-      toolbarColor: '#564dfc',
-    });
-  }
-
   navigateToBuyNumPage() {
     this.router.navigate(['wallets', 'buy-num']);
-  }
-
-  openAssetWalletHistory() {
-    this.diaBackendWalletService.assetWalletAddr$
-      .pipe(
-        first(),
-        switchMap(address =>
-          Browser.open({
-            url: `https://${this.domainMainnetNum}/address/${address}`,
-            toolbarColor: '#000000',
-          })
-        ),
-        untilDestroyed(this)
-      )
-      .subscribe();
-  }
-
-  refreshBalance(event: Event) {
-    (<CustomEvent>event).detail.complete();
-    this.networkConnected$
-      .pipe(
-        first(),
-        concatMap(networkConnected => {
-          if (!networkConnected) {
-            return this.errorService.toastError$(
-              this.translocoService.translate(
-                `error.wallets.noNetworkConnectionCannotRefreshBalance`
-              )
-            );
-          }
-          this.isLoadingBalance$.next(true);
-          return forkJoin([
-            this.diaBackendWalletService.syncAssetWalletBalance$(),
-            this.diaBackendAuthService.syncProfile$(),
-          ]).pipe(
-            tap(async ([assetWallet, _]) => {
-              this.totalBalance$.next(
-                Number(assetWallet[1]) +
-                  (await this.diaBackendAuthService.getPoints())
-              );
-            }),
-            catchError(() =>
-              this.errorService.toastError$(
-                this.translocoService.translate(
-                  `error.wallets.cannotRefreshBalance`
-                )
-              )
-            ),
-            finalize(() => this.isLoadingBalance$.next(false))
-          );
-        }),
-        untilDestroyed(this)
-      )
-      .subscribe();
-  }
-
-  async onDepositWithdrawBtnClick(mode: 'deposit' | 'withdraw') {
-    this.networkConnected$
-      .pipe(
-        first(),
-        concatMap(networkConnected => {
-          if (!networkConnected) {
-            return this.errorService.toastError$(
-              this.translocoService.translate(`error.internetError`)
-            );
-          }
-          return this.router.navigate(['wallets', 'transfer', mode]);
-        }),
-        untilDestroyed(this)
-      )
-      .subscribe();
   }
 
   async copyToClipboard(value: string) {
@@ -251,28 +123,5 @@ export class WalletsPage {
     this.snackBar.open(
       this.translocoService.translate('message.copiedToClipboard')
     );
-  }
-
-  exportIntegrityWalletPrivateKey() {
-    this.privateKey$
-      .pipe(
-        first(),
-        concatMap(async privateKey => {
-          const result = await this.confirmAlert.present({
-            message: this.translocoService.translate(
-              'message.confirmCopyPrivateKey'
-            ),
-          });
-          if (result)
-            this.dialog.open<ExportPrivateKeyModalComponent>(
-              ExportPrivateKeyModalComponent,
-              {
-                data: { privateKey },
-              }
-            );
-        }),
-        untilDestroyed(this)
-      )
-      .subscribe();
   }
 }
