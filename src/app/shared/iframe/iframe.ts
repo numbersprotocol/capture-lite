ccb3d8322b10678b7dff991291d6bbab1b127d25
sultanmyrza <sultanmyrza@gmail.com>
2023-02-22 12:18:39 +0800

    fix(wallet.page): unify iframe post messages

--- src/app/shared/iframe/iframe.ts
+++ src/app/shared/iframe/iframe.ts
@@ -3,6 +3,10 @@ export enum BubbleToIonicPostMessage {
   IFRAME_BACK_BUTTON_CLICKED = 'iframe-back-button-clicked',
   EDIT_CAPTION_CANCEL = 'edit-caption-cancel',
   EDIT_CAPTION_SAVE = 'edit-caption-save',
+  IFRAME_BUY_NUM_BUTTON_CLICKED = 'iframe-buy-num-button-clicked',
+  IFRAME_COPY_TO_CLIPBOARD_ASSET_WALLET = 'iframe-copy-to-clipboard-asset-wallet',
+  IFRAME_COPY_TO_CLIPBOARD_INTEGRITY_WALLET = 'iframe-copy-to-clipboard-integrity-wallet',
+  IFRAME_COPY_TO_CLIPBOARD_PRIVATE_KEY = 'iframe-copy-to-clipboard-private-key',
 }
 
 export enum IonicToBubblePostMessage {
