c4ab9d1ec82535171f5ec32aa635779f67322f88
sultanmyrza <sultanmyrza@gmail.com>
2023-04-07 17:40:11 +0800

    fix(set-secret.js): use PIPEDREAM_DELETE_CAPTURE_ACCOUNT instead of PIPEDREAM_URL

--- set-secret.js
+++ set-secret.js
@@ -11,7 +11,7 @@ export const BUBBLE_DB_URL = '${process.env.NUMBERS_BUBBLE_DB_URL}';
 export const BUBBLE_IFRAME_URL = '${process.env.NUMBERS_BUBBLE_IFRAME_URL}';
 export const BUBBLE_API_URL = '${process.env.BUBBLE_API_URL}';
 export const APPS_FLYER_DEV_KEY = '${process.env.APPS_FLYER_DEV_KEY}'
-export const PIPEDREAM_URL = '${process.env.PIPEDREAM_URL}'
+export const PIPEDREAM_DELETE_CAPTURE_ACCOUNT = '${process.env.PIPEDREAM_DELETE_CAPTURE_ACCOUNT}'
 `;
 fs.writeFile(targetPath, envConfigFile, err => {
   if (err) {
