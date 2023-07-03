6ecb9fef29754c8bf041ac51b6465e9d28236af1
sultanmyrza <sultanmyrza@gmail.com>
2023-07-03 13:12:34 +0800

    chore(memory.ts): remove unused method hasEnoughMemoryForEncoding

--- src/app/utils/memory.ts
+++ src/app/utils/memory.ts
@@ -14,30 +14,3 @@ export function calculateBase64Size(fileSizeInBytes: number): number {
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   return Math.ceil((fileSizeInBytes * 4) / 3);
 }
-
-/**
- * Checks if there is enough available memory to perform data encoding.
- *
- * @param fileSizeInBytes The size of the file in bytes.
- * @param availableMemory The available memory in bytes.
- * @param buffer The buffer size to add as a safety margin (default: 20% of fileSizeInBytes).
- *               The buffer provides additional memory space to handle any unforeseen memory
- *               requirements or fluctuations during the encoding process. It acts as a safety
- *               margin to avoid potential out-of-memory errors. By default, the buffer size is
- *               set to 20% of the fileSizeInBytes. You can adjust the buffer size based on your
- *               specific memory needs. Setting a larger buffer size increases the chances of
- *               successful encoding by allocating more memory, but it also consumes more memory
- *               resources. Finding the right balance between buffer size and available memory
- *               is important for optimal performance and reliability.
- * @returns True if there is enough memory, false otherwise.
- */
-export function hasEnoughMemoryForEncoding(
-  fileSizeInBytes: number,
-  availableMemory: number,
-  buffer = 0.2
-): boolean {
-  const base64SizeInBytes = calculateBase64Size(fileSizeInBytes);
-  const bufferSize = base64SizeInBytes * buffer;
-  const requiredMemory = base64SizeInBytes + bufferSize;
-  return requiredMemory <= availableMemory;
-}
