933e7ea4f2bfb51a62b4973d8271a6d1a36f73c0
sultanmyrza <sultanmyrza@gmail.com>
2023-08-03 14:46:11 +0800

    Fix: Show 30MB Limit Dialog for Media
    This commit displays a dialog when users select a video or image from the gallery, and its size exceeds 30MB. The informative message "File size exceeds 30MB..." will appear in such cases.
    
    However, when capturing media using the capture cam, the dialog won't be shown, regardless of the media's size, even if it exceeds 30MB.
    
    This enhancement keeps users informed about the file size limit, aiding them in making better decisions while selecting media from the gallery.

--- src/app/features/home/custom-camera/pre-publish-mode/pre-publish-mode.component.ts
+++ src/app/features/home/custom-camera/pre-publish-mode/pre-publish-mode.component.ts
@@ -7,6 +7,7 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
+import { CameraSource } from '@capacitor/camera';
 import { Directory, FilesystemPlugin } from '@capacitor/filesystem';
 import { AlertController, Platform } from '@ionic/angular';
 import { TranslocoService } from '@ngneat/transloco';
@@ -58,6 +59,12 @@ export class PrePublishModeComponent {
 
   private toggleBlackAndWhiteFilter = true;
 
+  readonly curCaptureCameraSource$ = new ReplaySubject<CameraSource>(1);
+
+  readonly isFromGallery$ = this.curCaptureCameraSource$.pipe(
+    map(cameraSource => cameraSource === CameraSource.Photos)
+  );
+
   readonly curCaptureFileSize$ = new ReplaySubject<number>(1);
 
   readonly curCaptureFilePath$ = new ReplaySubject<string>(1);
@@ -98,7 +105,12 @@ export class PrePublishModeComponent {
   readonly isFileSizeExceeded$ = combineLatest([
     this.curCaptureFileSize$,
     this.maxAllowedFileSize$,
-  ]).pipe(map(([curSize, maxSize]) => curSize < maxSize));
+  ]).pipe(map(([curSize, maxSize]) => curSize > maxSize));
+
+  @Input()
+  set curCaptureCameraSource(value: CameraSource | undefined) {
+    if (value) this.curCaptureCameraSource$.next(value);
+  }
 
   @Input()
   set curCaptureFileSize(value: number | undefined) {
@@ -227,17 +239,21 @@ export class PrePublishModeComponent {
       this.showIsFileSizeExceededModal()
     );
 
-    this.isFileSizeExceeded$
+    const shouldShowFileSizeExeededDialog$ = combineLatest([
+      this.isFromGallery$,
+      this.isFileSizeExceeded$,
+    ]).pipe(map(([c1, c2]) => c1 === true && c2 === true));
+
+    shouldShowFileSizeExeededDialog$
       .pipe(
         first(),
-        switchMap(hasEnoughMemory =>
+        switchMap(shouldShowFileSizeExeededDialog =>
           iif(
-            () => hasEnoughMemory,
-            runConfirmAction$,
-            showIsFileSizeExceededAction$
+            () => shouldShowFileSizeExeededDialog,
+            showIsFileSizeExceededAction$,
+            runConfirmAction$
           )
-        ),
-        catchError((error: unknown) => this.errorService.toastError$(error))
+        )
       )
       .subscribe();
   }
