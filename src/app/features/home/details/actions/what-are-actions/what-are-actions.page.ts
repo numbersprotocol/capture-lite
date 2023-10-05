038945e40f37d1148fae02371f375036cb98c013
sultanmyrza <sultanmyrza@gmail.com>
2023-10-05 18:57:51 +0800

    fix(what-are-actions.page): re-implement with ionic
    Due to bubble relase deadlines what-are-actions.page is re-implemented
    with ionic components instead of iframe + bubble page. In the future we
    might re-implement it with iframe  + bubble.

--- src/app/features/home/details/actions/what-are-actions/what-are-actions.page.ts
+++ src/app/features/home/details/actions/what-are-actions/what-are-actions.page.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { BehaviorSubject, fromEvent } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { BUBBLE_IFRAME_URL } from '../../../../../shared/dia-backend/secret';
-import { BubbleToIonicPostMessage } from '../../../../../shared/iframe/iframe';
-import { NetworkService } from '../../../../../shared/network/network.service';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
 @UntilDestroy()
 @Component({
@@ -13,37 +7,4 @@ import { NetworkService } from '../../../../../shared/network/network.service';
   templateUrl: './what-are-actions.page.html',
   styleUrls: ['./what-are-actions.page.scss'],
 })
-export class WhatAreActionsPage {
-  readonly networkConnected$ = this.networkService.connected$;
-  readonly iframeUrl = `${BUBBLE_IFRAME_URL}/what_are_actions`;
-  readonly iframeLoaded$ = new BehaviorSubject(false);
-
-  constructor(
-    private readonly networkService: NetworkService,
-    private readonly navController: NavController
-  ) {}
-
-  ionViewDidEnter() {
-    this.processIframeEvents();
-  }
-
-  processIframeEvents() {
-    fromEvent(window, 'message')
-      .pipe(
-        tap(event => {
-          const postMessageEvent = event as MessageEvent;
-          const data = postMessageEvent.data as BubbleToIonicPostMessage;
-          switch (data) {
-            case BubbleToIonicPostMessage.IFRAME_ON_LOAD:
-              this.iframeLoaded$.next(true);
-              break;
-            case BubbleToIonicPostMessage.IFRAME_BACK_BUTTON_CLICKED:
-              this.navController.back();
-              break;
-          }
-        }),
-        untilDestroyed(this)
-      )
-      .subscribe();
-  }
-}
+export class WhatAreActionsPage {}
