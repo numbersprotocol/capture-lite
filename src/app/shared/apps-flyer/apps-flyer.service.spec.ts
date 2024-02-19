f306704992f82296a24dd85487e1a86e79d6632c
sultanmyrza <sultanmyrza@gmail.com>
2024-02-19 20:42:47 +0800

    WIP: Add test for logging camera shutter event

--- src/app/shared/apps-flyer/apps-flyer.service.spec.ts
+++ src/app/shared/apps-flyer/apps-flyer.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { Platform } from '@ionic/angular';
 import { SharedTestingModule } from '../shared-testing.module';
 
+import { AFEvent } from 'appsflyer-capacitor-plugin';
 import { AppsFlyerService } from './apps-flyer.service';
 
 describe('AppsFlyerService', () => {
@@ -31,5 +32,27 @@ describe('AppsFlyerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log camera shutter event through Appsflyer.logEvent method', async () => {
+    const expectedEvent: AFEvent = {
+      eventName: 'af_level_achieved',
+      eventValue: {
+        af_level: '1',
+        af_score: '0',
+        af_achievement_id: 'ccam_try_click_camera_shutter',
+      },
+    };
+    // NOTE: We're accessing a private method here for testing purposes only.
+    // TypeScript doesn't enforce private/protected visibility at runtime, only at compile time.
+    // This is generally not recommended as it can lead to fragile tests and breaks encapsulation.
+    // However, in this case, we need to ensure that the private method 'logEvent' is called with
+    // the correct parameters.
+    const logEventSpy = spyOn(service as any, 'logEvent');
+
+    await service.logCameraShutterEvent();
+
+    expect(logEventSpy).toHaveBeenCalledWith(expectedEvent);
+    expect(logEventSpy).toHaveBeenCalledTimes(1);
   });
 });
