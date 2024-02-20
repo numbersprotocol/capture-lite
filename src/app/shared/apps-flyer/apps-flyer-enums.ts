19675d16d80e0d4a9f28cc8be6ed443247ed92a7
sultanmyrza <sultanmyrza@gmail.com>
2024-02-20 11:28:28 +0800

    Add JSDoc for AppsFlyer custom event enum

--- src/app/shared/apps-flyer/apps-flyer-enums.ts
+++ src/app/shared/apps-flyer/apps-flyer-enums.ts
@@ -1,3 +1,13 @@
+/**
+ * Enum representing custom event names for AppsFlyer in-app events.
+ *
+ * While it's recommended to use predefined event names, custom event names are easier for QA
+ * and marketing teams to understand and filter out in the AppsFlyer dashboard. Refer to the Asana
+ * comment https://app.asana.com/0/0/1206618451736620/1206636906894474/f for more details.
+ *
+ * Every custom event name should be prefixed with 'ccam_' which stands for Capture Cam. This prefix
+ * helps to distinguish between predefined AppsFlyer event names such as 'af_level_achieved', etc.
+ */
 export enum CCamCustomEventType {
   CCAM_TRY_CLICK_CAMERA_SHUTTER = 'ccam_try_click_camera_shutter',
 }
